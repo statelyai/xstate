628f304a5881f99dbe2cbd04849d60f69e3caf00
David Khourshid <davidkpiano@gmail.com>
2015-09-19 13:48:55 -0400

    Allowing array for transition symbols

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+exports['default'] = machine;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
@@ -30,8 +36,8 @@ var Machine = (function () {
     value: function transition(stateName) {
       var symbol = arguments.length <= 1 || arguments[1] === undefined ? null : arguments[1];
 
-      var state = this.states.find(function (_state) {
-        return _state.name === stateName;
+      var state = this.states.find(function (state) {
+        return state.name === stateName;
       });
 
       return this.transitionFn(state, symbol);
@@ -53,35 +59,62 @@ var State = function State(name) {
   });
 };
 
-var Transition = function Transition(fromState, toState, symbol) {
+var Transition = function Transition(fromState, toState, symbols) {
   _classCallCheck(this, Transition);
 
   this.from = fromState;
   this.to = toState;
-  this.symbol = symbol;
-};
+  this.symbols = symbols;
+}
+
+/**
+ * Simple machine representation for estado.
+ *
+ * {
+ *   (String) state : {
+ *     (String) toState : (String|String[]) symbol(s)
+ *   }
+ * }
+ * 
+ * @param  {Object} transitionGraph Object representing transition graph
+ *                                  (as described above)
+ * @param  {Array}  finalStates     Array of final states (defaults to [])
+ * @return {Machine}                Returns an estado Machine instance.
+ */
+;
 
 function machine(transitionGraph) {
+  var finalStates = arguments.length <= 1 || arguments[1] === undefined ? [] : arguments[1];
 
-  var states = Object.keys(transitionGraph).map(function (state) {
+  var states = Object.keys(transitionGraph).filter(function (state) {
+    return _lodash2['default'].isObject(transitionGraph[state]);
+  }).map(function (state) {
     return new State(state, transitionGraph[state]);
   });
 
-  var symbols = new Set(Object.keys(transitionGraph).map(function (state) {
-    return Object.values(transitionGraph[state]);
+  var symbols = _lodash2['default'].unique(Object.keys(transitionGraph).filter(function (state) {
+    return _lodash2['default'].isObject(transitionGraph[state]);
+  }).map(function (state) {
+    return _lodash2['default'].flatten(Object.values(transitionGraph[state]));
   }).reduce(function (a, b) {
     return a.concat(b);
   }));
 
   var initialState = states[0];
 
-  return new Machine(symbols, states, initialState, dfaTransition);
+  return new Machine(symbols, states, initialState, dfaTransition, finalStates);
 }
 
 function dfaTransition(state, symbol) {
-  return state.transitions.find(function (transition) {
-    return transition.symbol === symbol;
-  }).to;
+  var transition = state.transitions.find(function (transition) {
+    return _lodash2['default'].contains(transition.symbols, symbol);
+  });
+
+  if (transition) {
+    return transition.to;
+  }
+
+  console.error('State \'' + state.name + '\' does not have a valid transition for symbol \'' + symbol + '\'');
 }
 
 console.log(machine({
@@ -89,9 +122,11 @@ console.log(machine({
     yellow: 'timer'
   },
   yellow: {
-    red: 'timer'
+    red: ['timer', 'else']
   },
   red: {
-    green: 'timer'
-  }
-}).transition('red', 'timer'));
+    green: ['timer', 'a', 'b']
+  },
+  black: null
+}).transition('green', 'timer'));
+module.exports = exports['default'];
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Machine {
   }
 
   transition(stateName, symbol = null) {
-    let state = this.states.find((_state) => _state.name === stateName);
+    let state = this.states.find((state) => state.name === stateName);
 
     return this.transitionFn(state, symbol);
   }
@@ -27,31 +27,52 @@ class State {
 }
 
 class Transition {
-  constructor(fromState, toState, symbol) {
+  constructor(fromState, toState, symbols) {
     this.from = fromState;
     this.to = toState;
-    this.symbol = symbol;
+    this.symbols = symbols;
   }
 }
 
-function machine(transitionGraph) {
+/**
+ * Simple machine representation for estado.
+ *
+ * {
+ *   (String) state : {
+ *     (String) toState : (String|String[]) symbol(s)
+ *   }
+ * }
+ * 
+ * @param  {Object} transitionGraph Object representing transition graph
+ *                                  (as described above)
+ * @param  {Array}  finalStates     Array of final states (defaults to [])
+ * @return {Machine}                Returns an estado Machine instance.
+ */
+export default function machine(transitionGraph, finalStates = []) {
 
   let states = Object.keys(transitionGraph)
+    .filter((state) => _.isObject(transitionGraph[state]))
     .map((state) => new State(state, transitionGraph[state]));
 
-  let symbols = new Set(Object.keys(transitionGraph)
-    .map((state) => Object.values(transitionGraph[state]))
+  let symbols = _.unique(Object.keys(transitionGraph)
+    .filter((state) => _.isObject(transitionGraph[state]))
+    .map((state) => _.flatten(Object.values(transitionGraph[state])))
     .reduce((a, b) => a.concat(b)));
 
   let initialState = states[0];
 
-  return new Machine(symbols, states, initialState, dfaTransition);
+  return new Machine(symbols, states, initialState, dfaTransition, finalStates);
 }
 
 function dfaTransition(state, symbol) {
-  return state.transitions
-    .find((transition) => transition.symbol === symbol)
-    .to;
+  let transition = state.transitions
+    .find((transition) => _.contains(transition.symbols, symbol));
+
+  if (transition) {
+    return transition.to;
+  }
+
+  console.error(`State '${state.name}' does not have a valid transition for symbol '${symbol}'`);
 }
 
 console.log(machine({
@@ -59,9 +80,12 @@ console.log(machine({
     yellow: 'timer'
   },
   yellow: {
-    red: 'timer'
+    red: ['timer', 'else']
   },
   red: {
-    green: 'timer'
-  }
-}).transition('red', 'timer'));
+    green: ['timer', 'a', 'b']
+  },
+  black: null
+}).transition('green', 'timer'));
+
+
