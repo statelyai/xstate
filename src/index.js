e8fcf5fdc03cd9024d3c3f098870acb03092a35c
David Khourshid <davidkpiano@gmail.com>
2015-10-19 14:56:17 -0400

    Connecting utils to index and simplifying

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import machine from './dfa';
 import { machine as nfaMachine } from './nfa';
 import stateReducer from './state-reducer'
 import signalFilter from './utils/signal-filter';
+import mapState from './utils/map-state';
+import matchesState from './utils/matches-state';
 
 export {
   machine,
   nfaMachine,
   stateReducer,
-  signalFilter
+  signalFilter,
+  mapState,
+  matchesState
 };
