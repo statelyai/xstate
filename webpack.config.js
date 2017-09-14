94f3f86296dce44004ff035c12c792115eae11d4
David Khourshid <davidkpiano@gmail.com>
2017-09-14 17:45:22 -0400

    Removing rollup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './lib/index.js',
+  output: {
+    library: 'xstate',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+  // module: {
+  //   loaders: [
+  //     // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+  //     { test: /\.ts$/, loader: 'ts-loader' }
+  //   ]
+  // },
+  // plugins: []
+};
