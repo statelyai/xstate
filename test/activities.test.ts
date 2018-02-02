d26031ae6590a21ef34c7c8c6080b692ca45a963
David Khourshid <davidkpiano@gmail.com>
2018-02-02 00:02:56 -0500

    Adding activities tests

---
+++ test/activities.test.ts
@@ -0,0 +1,67 @@
+import { assert } from 'chai';
+import { Machine } from '../src/index';
+
+const lightMachine = Machine({
+  key: 'light',
+  initial: 'green',
+  states: {
+    green: {
+      on: {
+        TIMER: 'yellow'
+      }
+    },
+    yellow: {
+      on: {
+        TIMER: 'red'
+      }
+    },
+    red: {
+      initial: 'walk',
+      activities: ['activateCrosswalkLight'],
+      states: {
+        walk: { on: { PED_WAIT: 'wait' } },
+        wait: {
+          activities: ['blinkCrosswalkLight'],
+          on: { PED_STOP: 'stop' }
+        },
+        stop: {}
+      }
+    }
+  }
+});
+
+describe.only('activities', () => {
+  it('identifies start activities', () => {
+    assert.deepEqual(lightMachine.transition('yellow', 'TIMER').activities, {
+      start: ['activateCrosswalkLight'],
+      active: [],
+      stop: []
+    });
+  });
+
+  it('identifies start activities for child states and active activities', () => {
+    const redWalkState = lightMachine.transition('yellow', 'TIMER');
+    assert.deepEqual(
+      lightMachine.transition(redWalkState, 'PED_WAIT').activities,
+      {
+        start: ['blinkCrosswalkLight'],
+        active: ['activateCrosswalkLight'],
+        stop: []
+      }
+    );
+  });
+
+  it('identifies stop activities for child states', () => {
+    const redWalkState = lightMachine.transition('yellow', 'TIMER');
+    const redWaitState = lightMachine.transition(redWalkState, 'PED_WAIT');
+
+    assert.deepEqual(
+      lightMachine.transition(redWaitState, 'PED_STOP').activities,
+      {
+        start: [],
+        active: ['activateCrosswalkLight'],
+        stop: ['blinkCrosswalkLight']
+      }
+    );
+  });
+});
