41e814d39e1c57e6f65f1c9636a049c43753106a
David Khourshid <davidkpiano@gmail.com>
2018-03-01 10:11:34 -0500

    Adding send and cancel actions with delay. Resolves #36

--- src/actions.ts
+++ src/actions.ts
@@ -2,6 +2,7 @@ import {
   ActionObject,
   Action,
   ActionType,
+  Event,
   EventType,
   EventObject
 } from './types';
@@ -13,7 +14,9 @@ const PREFIX = 'xstate';
 export const actionTypes = {
   start: `${PREFIX}.start`,
   stop: `${PREFIX}.stop`,
-  raise: `${PREFIX}.raise`
+  raise: `${PREFIX}.raise`,
+  send: `${PREFIX}.send`,
+  cancel: `${PREFIX}.cancel`
 };
 
 export interface ActivityAction extends ActionObject {
@@ -38,10 +41,47 @@ const createActivityAction = (actionType: string) => (
   };
 };
 
+const toEventObject = (event: Event): EventObject => {
+  if (typeof event === 'string' || typeof event === 'number') {
+    return { type: event };
+  }
+
+  return event;
+};
+
 export const raise = (eventType: EventType): EventObject => ({
   type: actionTypes.raise,
   event: eventType
 });
+
+export interface SendAction extends ActionObject {
+  event: EventObject;
+  delay?: number;
+}
+export interface SendActionOptions {
+  delay?: number;
+  id?: string | number;
+}
+
+export const send = (event: Event, options?: SendActionOptions): SendAction => {
+  return {
+    type: actionTypes.send,
+    event: toEventObject(event),
+    delay: options ? options.delay : undefined,
+    id: options && options.id !== undefined ? options.id : getEventType(event)
+  };
+};
+
+export interface CancelAction extends ActionObject {
+  sendId: string | number;
+}
+
+export const cancel = (sendId: string | number): CancelAction => {
+  return {
+    type: actionTypes.cancel,
+    sendId
+  };
+};
 
 export const start = createActivityAction(actionTypes.start);
 export const stop = createActivityAction(actionTypes.stop);
--- test/actionCreators.test.ts
+++ test/actionCreators.test.ts
@@ -24,4 +24,46 @@ describe('action creators', () => {
       });
     });
   });
+
+  describe('send()', () => {
+    it('should accept a string event', () => {
+      const action = actions.send('foo');
+      assert.deepEqual(action, {
+        type: actionTypes.send,
+        event: { type: 'foo' },
+        delay: undefined,
+        id: 'foo'
+      });
+    });
+
+    it('should accept an event object', () => {
+      const action = actions.send({ type: 'foo', bar: 'baz' });
+      assert.deepEqual(action, {
+        type: actionTypes.send,
+        event: { type: 'foo', bar: 'baz' },
+        delay: undefined,
+        id: 'foo'
+      });
+    });
+
+    it('should accept an id option', () => {
+      const action = actions.send('foo', { id: 'foo-id' });
+      assert.deepEqual(action, {
+        type: actionTypes.send,
+        event: { type: 'foo' },
+        delay: undefined,
+        id: 'foo-id'
+      });
+    });
+
+    it('should accept a delay option', () => {
+      const action = actions.send('foo', { delay: 1000 });
+      assert.deepEqual(action, {
+        type: actionTypes.send,
+        event: { type: 'foo' },
+        delay: 1000,
+        id: 'foo'
+      });
+    });
+  });
 });
