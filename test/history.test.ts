9a3901743ebd698ae713dd9ff5fc9321d399ce20
Erik <erik@mogensoft.net>
2018-03-31 12:33:03 +0200

    Added tests that expose that the history mechanism works.
    It works for both shallow and deep states:
    
    $history is shallow
    $history.$history is 1 level deep
    $history.$history.$history is 2 levels deep, etc.
    
    Deep history fails if the node in question doesn't have substates,
    failing with the following error message:
    
    Error: Cannot read '$history' from state 'history.on.second.A': missing 'initial'

--- test/history.test.ts
+++ test/history.test.ts
@@ -44,3 +44,88 @@ describe('history states', () => {
     );
   });
 });
+
+describe('deep history states', () => {
+  const historyMachine = Machine({
+    key: 'history',
+    initial: 'off',
+    states: {
+      off: {
+        on: {
+          POWER: 'on.$history',
+          DEEP_POWER: 'on.$history.$history',
+          DEEPEST_POWER: 'on.$history.$history.$history'
+        }
+      },
+      on: {
+        initial: 'first',
+        states: {
+          first: {
+            on: { SWITCH: 'second' }
+          },
+          second: {
+            initial: 'A',
+            states: {
+              A: {
+                on: { INNER: 'B' }
+              },
+              B: {
+                initial: 'P',
+                states: {
+                  P: {
+                    on: { INNER: 'Q' }
+                  },
+                  Q: {}
+                }
+              }
+            }
+          }
+        },
+        on: {
+          POWER: 'off'
+        }
+      }
+    }
+  });
+
+  describe('$history', () => {
+    // on.first -> on.second.A
+    const state2A = historyMachine.transition({ on: 'first' }, 'SWITCH');
+    // on.second.A -> on.second.B.P
+    const state2BP = historyMachine.transition(state2A, 'INNER');
+    // on.second.B.P -> on.second.B.Q
+    const state2BQ = historyMachine.transition(state2BP, 'INNER');
+
+    it('should go to the shallow history', () => {
+      // on.second.B.P -> off
+      const stateOff = historyMachine.transition(state2BP, 'POWER');
+      assert.equal(
+        historyMachine.transition(stateOff, 'POWER').toString(),
+        'on.second.A'
+      );
+    });
+    it('should go to the deep history', () => {
+      // on.second.B.P -> off
+      const stateOff = historyMachine.transition(state2BP, 'POWER');
+      assert.equal(
+        historyMachine.transition(stateOff, 'DEEP_POWER').toString(),
+        'on.second.B.P'
+      );
+    });
+    it('should go to the deepest history', () => {
+      // on.second.B.Q -> off
+      const stateOff = historyMachine.transition(state2BQ, 'POWER');
+      assert.equal(
+        historyMachine.transition(stateOff, 'DEEPEST_POWER').toString(),
+        'on.second.B.Q'
+      );
+    });
+    it('can go to the shallow histor even when $history.$history is used', () => {
+      const stateOff = historyMachine.transition(state2A, 'POWER');
+      assert.equal(
+        historyMachine.transition(stateOff, 'DEEPEST_POWER').toString(),
+        'on.second.A'
+      );
+    });
+  });
+});
