0c30daddcd5215d6f79de938c258396f08585553
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-08-10 11:05:13 +0200

    Use rollup for UMD bundles

--- webpack.utils.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = {
-  output: {
-    library: 'xstateUtils',
-    libraryTarget: 'umd'
-  },
-  plugins: [new webpack.optimize.ModuleConcatenationPlugin()]
-};
