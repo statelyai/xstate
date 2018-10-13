a4451fa1a4554e89e77a167e074befd78e57e9ee
David Khourshid <davidkpiano@gmail.com>
2018-10-13 14:47:10 -0400

    Visualizer updates

--- docs/src/components/stateChart.jsx
+++ docs/src/components/stateChart.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import { interpret } from 'xstate/lib/interpreter';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import 'highlight.js/styles/monokai.css';
+import { Machine as _Machine } from 'xstate';
 
 const StyledMiniState = styled.div`
   padding: 0.25rem;
-  text-align: center;
+  text-align: left;
   border: 2px solid #dedede;
-  margin: 0.125rem;
+  margin: 0.5rem;
   flex-grow: 0;
   flex-shrink: 1;
   opacity: 0.8;
@@ -26,9 +29,29 @@ const StyledMiniState = styled.div`
     border-color: #57b0ea;
     opacity: 1;
   }
+
+  &[data-preview]:not([data-active]) {
+    outline: 1px solid green;
+  }
 `;
 
-const StyledEvent = styled.button`
+const StyledEvents = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`;
+
+const StyledEvent = styled.li`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-right: -1rem;
+  margin-bottom: 0.25rem;
+`;
+
+const StyledEventButton = styled.button`
   appearance: none;
   background: #57b0ea;
   color: white;
@@ -45,14 +68,25 @@ class StateChartNode extends React.Component {
     toggleStates: {}
   };
   render() {
-    const { stateNode, current, onEvent } = this.props;
+    const {
+      stateNode,
+      current,
+      preview,
+      onEvent,
+      onPreEvent,
+      onExitPreEvent
+    } = this.props;
     const isActive = current.matches(stateNode.path) || undefined;
+    const isPreview = preview
+      ? preview.matches(stateNode.path) || undefined
+      : undefined;
 
     return (
       <StyledMiniState
         key={stateNode.id}
         data-type={stateNode.type}
-        data-active={isActive}
+        data-active={isActive && stateNode.parent}
+        data-preview={isPreview && stateNode.parent}
         data-open={
           this.state.toggleStates[stateNode.id] === undefined
             ? true
@@ -80,25 +114,32 @@ class StateChartNode extends React.Component {
               <StateChartNode
                 stateNode={childStateNode}
                 current={current}
+                preview={preview}
                 key={childStateNode.id}
                 onEvent={onEvent}
+                onPreEvent={onPreEvent}
+                onExitPreEvent={onExitPreEvent}
               />
             );
           })}
         </div>
-        <div>
+        <StyledEvents>
           {stateNode.ownEvents.map(ownEvent => {
             const disabled = current.nextEvents.indexOf(ownEvent) === -1;
             return (
-              <StyledEvent
-                onClick={() => onEvent(ownEvent)}
-                disabled={disabled}
-              >
-                {ownEvent}
+              <StyledEvent>
+                <StyledEventButton
+                  onClick={() => onEvent(ownEvent)}
+                  onMouseOver={() => onPreEvent(ownEvent)}
+                  onMouseOut={() => onExitPreEvent()}
+                  disabled={disabled}
+                >
+                  {ownEvent}
+                </StyledEventButton>
               </StyledEvent>
             );
           })}
-        </div>
+        </StyledEvents>
       </StyledMiniState>
     );
   }
@@ -109,12 +150,13 @@ const StyledStateChart = styled.div`
   grid-template-columns: 50% 50%;
   grid-template-rows: auto;
   font-family: sans-serif;
-  font-size: 14px;
+  font-size: 12px;
 `;
 
 export class StateChart extends React.Component {
   state = {
-    current: this.props.machine.initialState
+    current: this.props.machine.initialState,
+    preview: undefined
   };
   interpreter = interpret(this.props.machine).onTransition(current => {
     this.setState({ current });
@@ -125,7 +167,7 @@ export class StateChart extends React.Component {
   }
   render() {
     const { machine } = this.props;
-    const { current } = this.state;
+    const { current, preview } = this.state;
 
     const stateNodes = machine.getStateNodes(current);
     const events = new Set();
@@ -150,21 +192,21 @@ export class StateChart extends React.Component {
           <StateChartNode
             stateNode={this.props.machine}
             current={current}
+            preview={preview}
             onEvent={this.interpreter.send.bind(this)}
+            onPreEvent={event =>
+              this.setState({ preview: this.interpreter.nextState(event) })
+            }
+            onExitPreEvent={() => this.setState({ preview: undefined })}
           />
-          <ul>
-            {current.nextEvents.map(nextEvent => {
-              return (
-                <button
-                  key={nextEvent}
-                  onClick={() => this.interpreter.send(nextEvent)}
-                >
-                  {nextEvent}
-                </button>
-              );
-            })}
-            <li>{JSON.stringify(current.value)}</li>
-          </ul>
+          <SyntaxHighlighter
+            language="json"
+            wrapLines={true}
+            useInlineStyles={false}
+            lineProps={line => ({ 'data-line': line })}
+          >
+            {JSON.stringify(machine.config, null, 2)}
+          </SyntaxHighlighter>
         </StyledStateChart>
       </section>
     );
