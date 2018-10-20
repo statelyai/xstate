651ef608d14dbeb31e1ec98b20daf5e77929e2b4
David Khourshid <davidkpiano@gmail.com>
2018-10-19 23:52:29 -0400

    Style updates

--- docs/src/components/layout.jsx
+++ docs/src/components/layout.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
+import { MDXProvider } from '@mdx-js/tag';
 import layoutStyles from './layout.module.css';
+import typeStyles from './typography.module.css';
+import 'prismjs/themes/prism-okaidia.css';
+import cn from 'classnames';
 
 // Highlight
 import hljs from 'highlight.js/lib/highlight';
@@ -39,13 +43,31 @@ class Layout extends React.Component {
               ]}
             >
               <html lang="en" />
+              <link
+                href="https://fonts.googleapis.com/css?family=Noto+Sans|Source+Code+Pro"
+                rel="stylesheet"
+              />
             </Helmet>
             <Header
               className={layoutStyles.header}
               siteTitle={data.site.siteMetadata.title}
             />
             <div className={layoutStyles.sidebar}>hello</div>
-            <div className={layoutStyles.content}>{children}</div>
+
+            <MDXProvider
+              components={{
+                h1: props => (
+                  <h1
+                    className={cn(typeStyles.heading, typeStyles.h1)}
+                    id={props.id}
+                  >
+                    {props.children}
+                  </h1>
+                )
+              }}
+            >
+              <main className={layoutStyles.content}>{children}</main>
+            </MDXProvider>
           </main>
         )}
       />
