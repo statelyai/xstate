8126350dfab1708e598f83f777102905d795ecd5
David Khourshid <davidkpiano@gmail.com>
2018-11-23 23:29:36 -0500

    VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'XState Docs',
+  description:
+    'Documentation for XState: State Machines and Statecharts for the Modern Web',
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'Guides',
+        children: [
+          '/guides/start',
+          '/guides/installation',
+          '/guides/states',
+          '/guides/transitions',
+          '/guides/hierarchical',
+          '/guides/parallel',
+          '/guides/actions',
+          '/guides/guards',
+          '/guides/context',
+          '/guides/activities',
+          '/guides/communication',
+          '/guides/delays',
+          '/guides/final',
+          '/guides/history',
+          '/guides/ids',
+          '/guides/internal',
+          '/guides/interpretation',
+          '/guides/typescript'
+        ]
+      }
+    ]
+  }
+};
