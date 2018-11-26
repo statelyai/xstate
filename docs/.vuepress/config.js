df8e8ea0a825c8d29f9428a763aaf1f647772ed2
David Khourshid <davidkpiano@gmail.com>
2018-11-25 21:03:47 -0500

    Revert "Remove docs/.vuepress"
    This reverts commit 64f81a3de6321bdbfdea835c23274a3ed1a5e473.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: 'XState Docs',
+  base: '/docs/',
+  description:
+    'Documentation for XState: State Machines and Statecharts for the Modern Web',
+  ga: 'UA-129726387-1',
+  themeConfig: {
+    logo: '/logo.svg',
+    nav: [
+      { text: 'API', link: 'https://xstate.js.org/api' },
+      { text: 'Visualizer', link: 'https://statecharts.github.io/xstate-viz' },
+      { text: 'Chat', link: 'https://gitter.im/statecharts/statecharts' },
+      { text: 'Community', link: 'https://spectrum.chat/statecharts' },
+      { text: 'GitHub', link: 'https://github.com/davidkpiano/xstate' }
+    ],
+    sidebar: [
+      {
+        title: 'Guides',
+        children: [
+          '/guides/start',
+          '/guides/installation',
+          '/guides/machines',
+          '/guides/states',
+          '/guides/transitions',
+          '/guides/hierarchical',
+          '/guides/parallel',
+          '/guides/actions',
+          '/guides/guards',
+          '/guides/context',
+          '/guides/activities',
+          '/guides/communication',
+          '/guides/delays',
+          '/guides/final',
+          '/guides/history',
+          '/guides/ids',
+          '/guides/internal',
+          '/guides/interpretation',
+          '/guides/typescript'
+        ]
+      },
+      {
+        title: 'Recipes',
+        children: ['/recipes/react', '/recipes/rxjs']
+      },
+      {
+        title: 'Examples',
+        children: ['/examples/todomvc']
+      }
+    ]
+  }
+};
