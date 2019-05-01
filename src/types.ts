72447b1c6f8bb9fc86cb8559136468b3b170bcb1
David Khourshid <davidkpiano@gmail.com>
2019-05-01 08:16:57 -0400

    (WIP) Add SCXML event type

--- src/types.ts
+++ src/types.ts
@@ -55,13 +55,13 @@ export type OmniEvent<TEvent extends EventObject> =
   | BuiltInEvent<TEvent>['type']
   | OmniEventObject<TEvent>;
 
-export interface ExecMeta<TContext, TEvent extends EventObject>
+export interface ActionMeta<TContext, TEvent extends EventObject>
   extends StateMeta<TContext, TEvent> {
   action: ActionObject<TContext, TEvent>;
 }
 
 export interface ActionFunction<TContext, TEvent extends EventObject> {
-  (context: TContext, event: TEvent, meta: ExecMeta<TContext, TEvent>):
+  (context: TContext, event: TEvent, meta: ActionMeta<TContext, TEvent>):
     | any
     | void;
   name: string;
@@ -880,6 +880,7 @@ export interface ValueAdjacencyMap<TContext, TEvent extends EventObject> {
 
 export interface StateMeta<TContext, TEvent extends EventObject> {
   state: State<TContext, TEvent>;
+  // _event: SCXML.Event;
 }
 
 export interface StateInterface<
@@ -942,4 +943,68 @@ export interface InterpreterOptions {
    * Default: `false`
    */
   devTools: boolean;
+}
+
+export namespace SCXML {
+  // tslint:disable-next-line:no-shadowed-variable
+  export interface Event {
+    /**
+     * This is a character string giving the name of the event.
+     * The SCXML Processor must set the name field to the name of this event.
+     * It is what is matched against the 'event' attribute of <transition>.
+     * Note that transitions can do additional tests by using the value of this field
+     * inside boolean expressions in the 'cond' attribute.
+     */
+    name: string;
+    /**
+     * This field describes the event type.
+     * The SCXML Processor must set it to: "platform" (for events raised by the platform itself, such as error events),
+     * "internal" (for events raised by <raise> and <send> with target '_internal')
+     * or "external" (for all other events).
+     */
+    type: 'platform' | 'internal' | 'external';
+    /**
+     * If the sending entity has specified a value for this, the Processor must set this field to that value
+     * (see C Event I/O Processors for details).
+     * Otherwise, in the case of error events triggered by a failed attempt to send an event,
+     * the Processor must set this field to the send id of the triggering <send> element.
+     * Otherwise it must leave it blank.
+     */
+    sendid: string | undefined;
+    /**
+     * This is a URI, equivalent to the 'target' attribute on the <send> element.
+     * For external events, the SCXML Processor should set this field to a value which,
+     * when used as the value of 'target', will allow the receiver of the event to <send>
+     * a response back to the originating entity via the Event I/O Processor specified in 'origintype'.
+     * For internal and platform events, the Processor must leave this field blank.
+     */
+    origin: string | undefined;
+    /**
+     * This is equivalent to the 'type' field on the <send> element.
+     * For external events, the SCXML Processor should set this field to a value which,
+     * when used as the value of 'type', will allow the receiver of the event to <send>
+     * a response back to the originating entity at the URI specified by 'origin'.
+     * For internal and platform events, the Processor must leave this field blank.
+     */
+    origintype: string | undefined;
+    /**
+     * If this event is generated from an invoked child process, the SCXML Processor
+     * must set this field to the invoke id of the invocation that triggered the child process.
+     * Otherwise it must leave it blank.
+     */
+    invokeid: string | undefined;
+    /**
+     * This field contains whatever data the sending entity chose to include in this event.
+     * The receiving SCXML Processor should reformat this data to match its data model,
+     * but must not otherwise modify it.
+     *
+     * If the conversion is not possible, the Processor must leave the field blank
+     * and must place an error 'error.execution' in the internal event queue.
+     */
+    data: Record<string, any> | undefined;
+    /**
+     * @private
+     */
+    $$type: 'scxml';
+  }
 }
