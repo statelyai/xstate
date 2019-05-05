e975be07329f220e0f2d9f49fc61d28fc4532d94
David Khourshid <davidkpiano@gmail.com>
2019-05-05 12:01:15 -0400

    WIP: each

---
+++ src/each.ts
@@ -0,0 +1,32 @@
+import { EventObject, SingleOrArray, ActionObject } from '.';
+
+export function each<TContext, TEvent extends EventObject>(
+  collection: keyof TContext,
+  item: keyof TContext,
+  actions: SingleOrArray<ActionObject<TContext, TEvent>>
+): ActionObject<TContext, TEvent>;
+export function each<TContext, TEvent extends EventObject>(
+  collection: keyof TContext,
+  item: keyof TContext,
+  index: keyof TContext,
+  actions: SingleOrArray<ActionObject<TContext, TEvent>>
+): ActionObject<TContext, TEvent>;
+export function each<TContext, TEvent extends EventObject>(
+  collection: keyof TContext,
+  item: keyof TContext,
+  indexOrActions:
+    | keyof TContext
+    | SingleOrArray<ActionObject<TContext, TEvent>>,
+  maybeActions?: SingleOrArray<ActionObject<TContext, TEvent>>
+): ActionObject<TContext, TEvent> {
+  const actions = maybeActions || indexOrActions;
+  const index = maybeActions ? indexOrActions : undefined;
+
+  return {
+    type: 'xstate.foreach',
+    collection,
+    item,
+    index,
+    actions
+  };
+}
