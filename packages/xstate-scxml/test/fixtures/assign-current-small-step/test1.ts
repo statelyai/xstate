5a4f4a475eb408ae1149f646aa2576edc5f4505f
David Khourshid <davidkpiano@gmail.com>
2019-10-28 19:57:07 -0400

    chore(@xstate/scxml): add assign-current-small-step tests test0 and test1

---
+++ packages/xstate-scxml/test/fixtures/assign-current-small-step/test1.ts
@@ -0,0 +1,44 @@
+import { Machine, assign } from 'xstate';
+import { log } from 'xstate/lib/actions';
+
+export default Machine<any>({
+  context: {
+    i: undefined
+  },
+  states: {
+    a: {
+      id: 'a',
+      on: {
+        t: {
+          target: 'b',
+          actions: assign({ i: 0 })
+        }
+      }
+    },
+    b: {
+      on: {
+        '': [
+          {
+            target: 'b',
+            cond: ctx => {
+              return ctx.i < 10;
+            },
+            actions: [
+              assign({
+                i: ctx => ctx.i + 1
+              }),
+              log()
+            ]
+          },
+          {
+            target: '#c',
+            cond: ctx => ctx.i === 10
+          }
+        ]
+      }
+    },
+    c: {
+      id: 'c'
+    }
+  }
+});
