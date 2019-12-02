2500d9792bd9be8bb1d9d50357bc219fbeef4015
David Khourshid <davidkpiano@gmail.com>
2019-12-02 13:16:46 -0500

    chore(core): remove devtools implementation (will be added by extension)

--- packages/core/src/devTools.ts
+++ packages/core/src/devTools.ts
@@ -10,36 +10,23 @@ interface DevInterface {
 
 type ServicesListener = (service: Set<AnyInterpreter>) => void;
 
-function initDevTools(): DevInterface | undefined {
-  if (IS_PRODUCTION || typeof window === 'undefined') return;
-
+function getDevTools(): DevInterface | undefined {
   const w = window as Window & { __xstate__?: DevInterface };
-  const services = new Set<Interpreter<any, any, any>>();
-  const serviceListeners = new Set<ServicesListener>();
+  if (!!w.__xstate__) {
+    return w.__xstate__;
+  }
 
-  w.__xstate__ = w.__xstate__ || {
-    services,
-    register(service) {
-      services.add(service);
-      serviceListeners.forEach(listener => listener(services));
-    },
-    onRegister(listener) {
-      serviceListeners.add(listener);
-      listener(services);
-    }
-  };
-
-  return w.__xstate__;
-}
-
-if (!IS_PRODUCTION) {
-  initDevTools();
+  return undefined;
 }
 
 export function registerService(service: AnyInterpreter) {
-  if (IS_PRODUCTION || typeof window === 'undefined') return;
+  if (IS_PRODUCTION || typeof window === 'undefined') {
+    return;
+  }
 
-  const dev = initDevTools();
+  const devTools = getDevTools();
 
-  dev && dev.register(service);
+  if (devTools) {
+    devTools.register(service);
+  }
 }
