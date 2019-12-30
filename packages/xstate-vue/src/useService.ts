959982c64c674925a1854391d5d28ff846e60015
Alexander von Studnitz <alexander.von.studnitz@gmail.com>
2019-12-30 02:07:00 +0100

    Add useService tests

--- packages/xstate-vue/src/useService.ts
+++ packages/xstate-vue/src/useService.ts
@@ -1,30 +1,35 @@
-import { ref, Ref, onBeforeUnmount } from '@vue/composition-api';
+import { ref, Ref, watch, isRef } from '@vue/composition-api';
 import { EventObject, State, Interpreter } from 'xstate';
 
 export function useService<TContext, TEvent extends EventObject>(
-  service: Interpreter<TContext, any, TEvent>
+  service:
+    | Interpreter<TContext, any, TEvent>
+    | Ref<Interpreter<TContext, any, TEvent>>
 ): {
   current: Ref<State<TContext, TEvent>>;
   send: Interpreter<TContext, any, TEvent>['send'];
-  service: Interpreter<TContext, any, TEvent>;
+  service: Ref<Interpreter<TContext, any, TEvent>>;
 } {
-  const current = ref<State<TContext, TEvent>>(service.state);
+  const serviceRef = isRef(service)
+    ? service
+    : ref<Interpreter<TContext, any, TEvent>>(service);
+  const current = ref<State<TContext, TEvent>>(serviceRef.value.state);
 
-  const { unsubscribe } = service.subscribe(state => {
-    if (state.changed) {
-      current.value = state;
-    }
+  watch(serviceRef, (service, _, onCleanup) => {
+    current.value = serviceRef.value.state;
+    const { unsubscribe } = service.subscribe(state => {
+      if (state.changed) {
+        current.value = state;
+      }
+    });
+    onCleanup(() => unsubscribe());
   });
 
-  const send = (event: TEvent | TEvent['type']) => service.send(event);
-
-  onBeforeUnmount(() => {
-    unsubscribe();
-  });
+  const send = (event: TEvent | TEvent['type']) => serviceRef.value.send(event);
 
   return {
     current,
     send,
-    service
+    service: serviceRef
   };
 }
