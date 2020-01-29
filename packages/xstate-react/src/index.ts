1f05fddb08ab08940ef75a3e0dbeb300d9e0e8a4
David Khourshid <davidkpiano@gmail.com>
2020-01-29 07:55:47 -0500

    fix(@xstate/react): warn instead of throwing an error for changed machines

--- packages/xstate-react/src/index.ts
+++ packages/xstate-react/src/index.ts
@@ -42,8 +42,8 @@ export function useMachine<
   if (process.env.NODE_ENV !== 'production') {
     const [initialMachine] = useState(machine);
 
-    if (machine !== initialMachine) {
-      throw new Error(
+    if (process.env.NODE_ENV !== 'production' && machine !== initialMachine) {
+      console.warn(
         'Machine given to `useMachine` has changed between renders. This is not supported and might lead to unexpected results.\n' +
           'Please make sure that you pass the same Machine as argument each time.'
       );
