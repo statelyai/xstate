a8d8278997f925751a58bdda92debb4f157bc26f
David Khourshid <davidkpiano@gmail.com>
2020-02-22 10:36:35 -0500

    Removing dead code

--- packages/core/src/StateNode.ts
+++ packages/core/src/StateNode.ts
@@ -134,10 +134,6 @@ export class StateNode<
 
   protected __cache = {
     events: undefined as Array<TEvent['type']> | undefined,
-    relativeValue: new Map() as Map<
-      StateNode<TContext, any, TEvent>,
-      StateValue
-    >,
     initialStateValue: undefined as StateValue | undefined,
     initialState: undefined as State<TContext, TEvent> | undefined,
     on: undefined as TransitionDefinitionMap<TContext, TEvent> | undefined,
