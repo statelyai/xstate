2474e40721ed50de94f15b19ca11fc8ac468fbe6
David Khourshid <davidkpiano@gmail.com>
2020-03-19 12:56:24 -0400

    Update current value

--- packages/xstate-react/src/ActorRef.ts
+++ packages/xstate-react/src/ActorRef.ts
@@ -1,4 +1,4 @@
-import { EventObject, Interpreter, State } from 'xstate';
+import { EventObject, State } from 'xstate';
 
 export interface Unsubscribable {
   unsubscribe(): any | void;
@@ -25,69 +25,78 @@ export interface ActorRef<TCurrent, TEvent extends EventObject>
 export type Sender<TEvent extends EventObject> = (event: TEvent) => void;
 
 export class ActorRef<TCurrent, TEvent extends EventObject, TRef = any> {
+  private subscription: Unsubscribable;
   constructor(
     public send: Sender<TEvent>,
     public subscribe: Subscribable<TCurrent>['subscribe'],
     public current: TCurrent,
     public ref: TRef
-  ) {}
-
-  public static fromObservable<T>(
-    observable: Subscribable<T>
-  ): ActorRef<T | undefined, never> {
-    return new ActorRef<T | undefined, never, typeof observable>(
-      () => void 0,
-      observable.subscribe.bind(observable),
-      undefined,
-      observable
-    );
+  ) {
+    this.subscription = subscribe(latest => {
+      this.current = latest;
+    });
   }
 
-  public static fromPromise<T>(
-    promise: PromiseLike<T>
-  ): ActorRef<T | undefined, never> {
-    return new ActorRef<T | undefined, never, typeof promise>(
-      () => void 0,
-      (next, handleError, complete) => {
-        let unsubscribed = false;
-        promise.then(
-          response => {
-            if (unsubscribed) {
-              return;
-            }
-            next && next(response);
-            if (unsubscribed) {
-              return;
-            }
-            complete && complete();
-          },
-          err => {
-            if (unsubscribed) {
-              return;
-            }
-            handleError && handleError(err);
+  public stop() {
+    this.subscription.unsubscribe();
+  }
+}
+
+export function fromObservable<T>(
+  observable: Subscribable<T>
+): ActorRef<T | undefined, never> {
+  return new ActorRef<T | undefined, never, typeof observable>(
+    () => void 0,
+    observable.subscribe.bind(observable),
+    undefined,
+    observable
+  );
+}
+
+export function fromPromise<T>(
+  promise: PromiseLike<T>
+): ActorRef<T | undefined, never> {
+  return new ActorRef<T | undefined, never, typeof promise>(
+    () => void 0,
+    (next, handleError, complete) => {
+      let unsubscribed = false;
+      promise.then(
+        response => {
+          if (unsubscribed) {
+            return;
           }
-        );
+          next && next(response);
+          if (unsubscribed) {
+            return;
+          }
+          complete && complete();
+        },
+        err => {
+          if (unsubscribed) {
+            return;
+          }
+          handleError && handleError(err);
+        }
+      );
 
-        return {
-          unsubscribe: () => (unsubscribed = true)
-        };
-      },
-      undefined,
-      promise
-    );
-  }
+      return {
+        unsubscribe: () => (unsubscribed = true)
+      };
+    },
+    undefined,
+    promise
+  );
+}
 
-  public static fromService<TContext, TEvent extends EventObject>(
-    service: ActorLike<State<TContext, TEvent>, TEvent> & {
-      state: State<TContext, TEvent>;
-    }
-  ): ActorRef<State<TContext, TEvent>, TEvent, typeof service> {
-    return new ActorRef(
-      service.send.bind(service),
-      service.subscribe.bind(service),
-      service.state,
-      service
-    );
+export function fromService<TContext, TEvent extends EventObject>(
+  service: ActorLike<State<TContext, TEvent>, TEvent> & {
+    state: State<TContext, TEvent>;
   }
+): ActorRef<State<TContext, TEvent>, TEvent, typeof service> {
+  return new ActorRef(
+    service.send.bind(service),
+    service.subscribe.bind(service),
+    service.state,
+    service
+  );
 }
