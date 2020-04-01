6edce06d3c740c8b1966b148725e79c8e9dd6183
Dmytro Mostovyi <djmakaron2009@gmail.com>
2020-04-01 23:51:35 +0300

    Add UMD bundle for @xstate/vue

---
+++ packages/xstate-vue/rollup.config.js
@@ -0,0 +1,52 @@
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
+function createTSCofig() {
+  return typescript({
+    tsconfigOverride: {
+      compilerOptions: {
+        declaration: false
+      }
+    }
+  });
+}
+
+function createUmdConfig({ input, output: file, name }) {
+  return {
+    input,
+    output: {
+      file,
+      format: 'umd',
+      name,
+      globals: {
+        xstate: 'XState',
+        '@xstate/fsm': 'XStateFSM',
+        '@vue/composition-api': 'vueCompositionApi'
+      }
+    },
+    plugins: [createTSCofig(), terser({ include: [/^.+\.min\.js$/] })]
+  };
+}
+
+export default [
+  createUmdConfig({
+    name: 'XStateVue',
+    input: 'src/index.ts',
+    output: 'dist/xstate-vue.js'
+  }),
+  createUmdConfig({
+    name: 'XStateVue',
+    input: 'src/index.ts',
+    output: 'dist/xstate-vue.min.js'
+  }),
+  createUmdConfig({
+    name: 'XStateVueFSM',
+    input: 'src/fsm.ts',
+    output: 'dist/xstate-vue.fsm.js'
+  }),
+  createUmdConfig({
+    name: 'XStateVueFSM',
+    input: 'src/fsm.ts',
+    output: 'dist/xstate-vue.fsm.min.js'
+  })
+];
