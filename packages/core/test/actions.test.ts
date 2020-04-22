e16e48e05e6243a3eacca58a13d3e663cd641f55
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-04-22 11:45:26 +0200

    Fixed an issue with `choose` and `pure` not being able to use actions defined in options

--- packages/core/test/actions.test.ts
+++ packages/core/test/actions.test.ts
@@ -1292,4 +1292,31 @@ describe('choose', () => {
 
     expect(service.state.context).toEqual({ counter: 101, answer: 42 });
   });
+
+  it('should be able to use actions defined in options', () => {
+    interface Ctx {
+      answer?: number;
+    }
+
+    const machine = createMachine<Ctx>(
+      {
+        context: {},
+        initial: 'foo',
+        states: {
+          foo: {
+            entry: choose([{ cond: () => true, actions: 'revealAnswer' }])
+          }
+        }
+      },
+      {
+        actions: {
+          revealAnswer: assign<Ctx>({ answer: 42 })
+        }
+      }
+    );
+
+    const service = interpret(machine).start();
+
+    expect(service.state.context).toEqual({ answer: 42 });
+  });
 });
