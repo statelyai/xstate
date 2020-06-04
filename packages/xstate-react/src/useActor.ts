b6ef76f82396a6ee282199fce71e0e6338bad6e3
David Khourshid <davidkpiano@gmail.com>
2020-06-04 08:36:21 -0400

    Remove getCurrentValue

--- packages/xstate-react/src/useActor.ts
+++ packages/xstate-react/src/useActor.ts
@@ -1,20 +1,11 @@
 import { useState, useEffect } from 'react';
-import { ActorRefLike, ActorRef, Sender } from './types';
+import { ActorRef, Sender } from './types';
 import { EventObject } from 'xstate';
 
-const defaultGetCurrentValue = <TEvent extends EventObject, TEmitted = any>(
-  actorLike: ActorRef<TEvent, TEmitted> | ActorRefLike<TEvent, TEmitted>
-): TEmitted => {
-  return 'current' in actorLike ? actorLike.current : (undefined as any); // V4 Actors do not have 'current'
-};
-
 export function useActor<TEvent extends EventObject, TEmitted = any>(
-  actorLike: ActorRef<TEvent, TEmitted> | ActorRefLike<TEvent, TEmitted>,
-  getCurrentValue: <TEvent extends EventObject, TEmitted = any>(
-    actorLike: ActorRef<TEvent, TEmitted> | ActorRefLike<TEvent, TEmitted>
-  ) => TEmitted = defaultGetCurrentValue
+  actorLike: ActorRef<TEvent, TEmitted>
 ): [TEmitted, Sender<TEvent>] {
-  const [current, setCurrent] = useState(getCurrentValue(actorLike));
+  const [current, setCurrent] = useState(actorLike.current);
 
   useEffect(() => {
     const subscription = actorLike.subscribe(setCurrent);
