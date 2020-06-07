421303cbb7c1d1ce5f9f13d0180f1877445fb30e
David Khourshid <davidkpiano@gmail.com>
2020-06-06 23:16:31 -0400

    Fix styles

--- packages/xstate-viz/stories/MachineViz.stories.tsx
+++ packages/xstate-viz/stories/MachineViz.stories.tsx
@@ -29,3 +29,82 @@ export const SimpleMachine = () => {
     <MachineViz machine={simpleMachine} state={simpleMachine.initialState} />
   );
 };
+
+const donutMachine = createMachine({
+  id: 'donut',
+  initial: 'ingredients',
+  states: {
+    ingredients: {
+      on: {
+        NEXT: 'directions'
+      }
+    },
+    directions: {
+      initial: 'makeDough',
+      onDone: 'fry',
+      states: {
+        makeDough: {
+          on: { NEXT: 'mix' }
+        },
+        mix: {
+          type: 'parallel',
+          states: {
+            mixDry: {
+              initial: 'mixing',
+              states: {
+                mixing: {
+                  on: { MIXED_DRY: 'mixed' }
+                },
+                mixed: {
+                  type: 'final'
+                }
+              }
+            },
+            mixWet: {
+              initial: 'mixing',
+              states: {
+                mixing: {
+                  on: { MIXED_WET: 'mixed' }
+                },
+                mixed: {
+                  type: 'final'
+                }
+              }
+            }
+          },
+          onDone: 'allMixed'
+        },
+        allMixed: {
+          type: 'final'
+        }
+      }
+    },
+    fry: {
+      on: {
+        NEXT: 'flip'
+      }
+    },
+    flip: {
+      on: {
+        NEXT: 'dry'
+      }
+    },
+    dry: {
+      on: {
+        NEXT: 'glaze'
+      }
+    },
+    glaze: {
+      on: {
+        NEXT: 'serve'
+      }
+    },
+    serve: {}
+  }
+});
+
+export const DonutMachine = () => {
+  return (
+    <MachineViz machine={donutMachine} state={donutMachine.initialState} />
+  );
+};
