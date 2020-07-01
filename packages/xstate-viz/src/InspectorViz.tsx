6f6163bf4f0a871a04bcaf4774991e49a5ce0ee7
David Khourshid <davidkpiano@gmail.com>
2020-06-30 22:19:56 -0400

    Style updates

--- packages/xstate-viz/src/InspectorViz.tsx
+++ packages/xstate-viz/src/InspectorViz.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { createMachine, State, StateNode, SCXML, Interpreter } from 'xstate';
 import { useMachine } from '@xstate/react';
 import { MachineViz } from './MachineViz';
-import { assign } from '@xstate/immer';
+import { assign, createUpdater, ImmerUpdateEvent } from '@xstate/immer';
 // import { EventRecordsViz } from './EventRecordsViz';
 import { StateViz } from './StateViz';
 import { createContext } from 'react';
@@ -14,6 +14,15 @@ const parseState = (stateJSON: string): State<any, any> => {
 
   return state;
 };
+
+type ViewUpdateEvent = ImmerUpdateEvent<'view.update', 'graph' | 'state'>;
+
+const viewUpdater = createUpdater<InspectorCtx, ViewUpdateEvent>(
+  'view.update',
+  (ctx, { input }) => {
+    ctx.view = input;
+  }
+);
 
 type InspectorEvent =
   | {
@@ -31,7 +40,8 @@ type InspectorEvent =
   | {
       type: 'service.select';
       id: string;
-    };
+    }
+  | ViewUpdateEvent;
 
 interface ServiceDataCtx {
   state: State<any, any>;
@@ -43,13 +53,15 @@ interface ServiceDataCtx {
 interface InspectorCtx {
   services: Record<string, ServiceDataCtx>;
   service?: string;
+  view: 'graph' | 'state';
 }
 
 const inspectorMachine = createMachine<InspectorCtx, InspectorEvent>({
   id: 'inspector',
   context: {
     services: {},
-    service: undefined
+    service: undefined,
+    view: 'graph'
   },
   initial: 'pending',
   states: {
@@ -95,7 +107,8 @@ const inspectorMachine = createMachine<InspectorCtx, InspectorEvent>({
         };
       }),
       target: '.inspecting'
-    }
+    },
+    [viewUpdater.type]: { actions: viewUpdater.action }
   }
 });
 
@@ -128,26 +141,44 @@ export const InspectorViz: React.FC = () => {
 
   return (
     <ServicesContext.Provider value={service}>
-      <div data-xviz="inspector">
-        <div data-xviz="services">
-          {Object.keys(state.context.services).map((key) => {
-            return (
-              <div
-                data-xviz="service-link"
-                key={key}
-                onClick={() => send({ type: 'service.select', id: key })}
-              >
-                {key}
-              </div>
-            );
-          })}
-        </div>
+      <div data-xviz="inspector" data-xviz-view={state.context.view}>
+        <header data-xviz="inspector-header">
+          <select
+            onChange={(e) => {
+              send({ type: 'service.select', id: e.target.value });
+            }}
+            value={state.context.service}
+            data-xviz="inspector-action"
+          >
+            {Object.keys(state.context.services).map((key) => {
+              return (
+                <option data-xviz="service-link" key={key} value={key}>
+                  {key}
+                </option>
+              );
+            })}
+          </select>
+
+          <button
+            data-xviz="inspector-action"
+            onClick={() => send(viewUpdater.update('graph'))}
+          >
+            Graph
+          </button>
+          <button
+            data-xviz="inspector-action"
+            onClick={() => send(viewUpdater.update('state'))}
+          >
+            State
+          </button>
+        </header>
 
         {Object.entries(state.context.services).map(([key, service]) => {
           return (
             <ServiceDataContext.Provider value={service} key={key}>
               <div
                 data-xviz="service"
+                data-xviz-view={state.context.view}
                 hidden={currentService !== service || undefined}
               >
                 <MachineViz machine={service.machine} state={service.state} />
