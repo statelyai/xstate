4e949ec856349062352562c825beb0654e528f81
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-08-21 15:23:42 +0200

    Fixed an issue with spawning actors multiple times when they got spawned in the nested initial state (#1401)

---
+++ packages/core/src/serviceScope.ts
@@ -0,0 +1,22 @@
+import { Interpreter } from './interpreter';
+
+/**
+ * Maintains a stack of the current service in scope.
+ * This is used to provide the correct service to spawn().
+ */
+
+const serviceStack = [] as Array<Interpreter<any, any, any> | undefined>;
+
+export const provide = <T, TService extends Interpreter<any, any, any>>(
+  service: TService | undefined,
+  fn: (service: TService | undefined) => T
+) => {
+  serviceStack.push(service);
+  const result = fn(service);
+  serviceStack.pop();
+  return result;
+};
+
+export const consume = <T, TService extends Interpreter<any, any, any>>(
+  fn: (service: TService) => T
+) => fn(serviceStack[serviceStack.length - 1] as TService);
