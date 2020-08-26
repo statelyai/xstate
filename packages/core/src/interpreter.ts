c02ccbde2d80c6e52c526fde73a9520379d4c8b3
Tom Enden <tome@wix.com>
2020-08-26 13:15:51 +0300

    use self instead of window to simplify the code

--- packages/core/src/interpreter.ts
+++ packages/core/src/interpreter.ts
@@ -124,13 +124,7 @@ export class Interpreter<
     },
     logger: global.console.log.bind(console),
     devTools: false
-  }))(
-    typeof window !== 'undefined'
-      ? window
-      : typeof self !== 'undefined'
-      ? self
-      : global
-  );
+  }))(typeof self !== 'undefined' ? self : global);
   /**
    * The current state of the interpreted machine.
    */
