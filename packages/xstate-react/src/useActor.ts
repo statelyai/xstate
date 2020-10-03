f61ae9e486d05295b9ff8bbf395738643f593356
David Khourshid <davidkpiano@gmail.com>
2020-10-03 16:53:55 -0400

    Stabilize `send` from useActor

--- packages/xstate-react/src/useActor.ts
+++ packages/xstate-react/src/useActor.ts
@@ -1,31 +1,31 @@
-import { useState, useEffect, useRef, useCallback } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Sender, ActorRefLike } from './types';
 import { EventObject, Actor } from 'xstate';
+import useConstant from './useConstant';
 
 export function useActor<TEvent extends EventObject, TEmitted = any>(
   actorRef: ActorRefLike<TEvent, TEmitted> | Actor,
   getSnapshot: (actor: typeof actorRef) => TEmitted = (a) =>
     'state' in a ? a.state : (undefined as any)
 ): [TEmitted, Sender<TEvent>] {
-  // const actor = useMemo(() => resolveActor(actorLike), [actorLike]);
+  const actorRefRef = useRef(actorRef);
   const deferredEventsRef = useRef<TEvent[]>([]);
   const [current, setCurrent] = useState(() => getSnapshot(actorRef));
 
-  const send: Sender<TEvent> = useCallback(
-    (event) => {
-      // If the previous actor is a deferred actor,
-      // queue the events so that they can be replayed
-      // on the non-deferred actor.
-      if ('deferred' in actorRef && actorRef.deferred) {
-        deferredEventsRef.current.push(event);
-      } else {
-        actorRef.send(event);
-      }
-    },
-    [actorRef]
-  );
+  const send: Sender<TEvent> = useConstant(() => (event) => {
+    const currentActorRef = actorRefRef.current;
+    // If the previous actor is a deferred actor,
+    // queue the events so that they can be replayed
+    // on the non-deferred actor.
+    if ('deferred' in currentActorRef && currentActorRef.deferred) {
+      deferredEventsRef.current.push(event);
+    } else {
+      currentActorRef.send(event);
+    }
+  });
 
   useEffect(() => {
+    actorRefRef.current = actorRef;
     setCurrent(getSnapshot(actorRef));
     const subscription = actorRef.subscribe(setCurrent);
 
