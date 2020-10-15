4c7ceb99ff367b0bb844e2bcd4db74bc2372e2bb
David Khourshid <davidkpiano@gmail.com>
2020-10-15 08:28:51 -0400

    Fix typings for createModel

--- packages/core/src/model.ts
+++ packages/core/src/model.ts
@@ -2,34 +2,43 @@ import { assign } from './actions';
 import { AssignAction, Assigner, EventObject, PropertyAssigner } from './types';
 import { mapValues } from './utils';
 
-export function createModel<TContext, TEvent extends EventObject>(
-  initialState: TContext
-): {
-  context: TContext | (() => TContext);
+export interface ContextModel<TC, TE extends EventObject> {
+  context: TC | (() => TC);
+  actions: {
+    [key: string]: Assigner<TC, TE> | PropertyAssigner<TC, TE>;
+  };
   withAssigners: <
     T extends {
-      [key: string]:
-        | Assigner<TContext, TEvent>
-        | PropertyAssigner<TContext, TEvent>;
+      [key: string]: Assigner<TC, TE> | PropertyAssigner<TC, TE>;
     }
   >(
     assigners: T
-  ) => {
-    context: TContext;
+  ) => ContextModel<TC, TE> & {
     actions: {
-      [K in keyof T]: AssignAction<TContext, TEvent>;
+      [K in keyof T]: AssignAction<TC, TE>;
     };
   };
-} {
-  return {
+}
+
+export function createModel<TContext, TEvent extends EventObject>(
+  initialState: TContext | (() => TContext)
+): ContextModel<TContext, TEvent> {
+  const model: ContextModel<TContext, TEvent> = {
     context: initialState,
+    actions: {},
     withAssigners: (assigners) => {
       return {
-        context: initialState,
+        ...model,
         actions: mapValues(assigners, (assignment) => {
           return assign(assignment);
         })
+      } as ContextModel<TContext, TEvent> & {
+        actions: {
+          [K in keyof typeof assigners]: AssignAction<TContext, TEvent>;
+        };
       };
     }
   };
+
+  return model;
 }
