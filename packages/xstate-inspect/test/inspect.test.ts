5f8fbf685bde2311dcc25ebf7e1c338174d2301b
David Khourshid <davidkpiano@gmail.com>
2020-12-14 07:25:21 -0500

    Add test for events with circular structurs

--- packages/xstate-inspect/test/inspect.test.ts
+++ packages/xstate-inspect/test/inspect.test.ts
@@ -2,7 +2,7 @@ import { createMachine, interpret } from 'xstate';
 import { createDevTools, inspect } from '../src';
 
 describe('@xstate/inspect', () => {
-  it('should handle circular structures', (done) => {
+  it('should handle circular structures in context', (done) => {
     const circularStructure = {
       get cycle() {
         return circularStructure;
@@ -29,6 +29,51 @@ describe('@xstate/inspect', () => {
     });
 
     const service = interpret(machine).start();
+
+    // The devTools will notify the listeners:
+    // 1. the built-in service listener
+    // 2. the test listener that calls done() above
+    // with the service. The built-in service listener is responsible for
+    // stringifying the service's machine definition (which contains a circular structure)
+    // and will throw an error if circular structures are not handled.
+    expect(() => devTools.register(service)).not.toThrow();
+  });
+
+  it('should handle circular structures in events', (done) => {
+    const circularStructure = {
+      get cycle() {
+        return circularStructure;
+      }
+    };
+
+    const machine = createMachine({
+      initial: 'active',
+      states: {
+        active: {}
+      }
+    });
+
+    const devTools = createDevTools();
+
+    devTools.onRegister((inspectedService) => {
+      inspectedService.onTransition((state) => {
+        if (state.event.type === 'CIRCULAR') {
+          done();
+        }
+      });
+    });
+
+    inspect({
+      iframe: false,
+      devTools
+    });
+
+    const service = interpret(machine).start();
+
+    service.send({
+      type: 'CIRCULAR',
+      value: circularStructure
+    });
 
     // The devTools will notify the listeners:
     // 1. the built-in service listener
