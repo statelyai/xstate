60b2fb13ed9ad3393090104421ed6383bb5a5c10
David Khourshid <davidkpiano@gmail.com>
2020-12-21 08:31:26 -0500

    Remove actions property

--- packages/core/src/model.ts
+++ packages/core/src/model.ts
@@ -1,6 +1,5 @@
 import { assign } from './actions';
 import type {
-  ActionObject,
   AssignAction,
   Assigner,
   PropertyAssigner,
@@ -10,9 +9,6 @@ import type {
 
 export interface ContextModel<TContext, TEvent extends EventObject> {
   initialContext: TContext;
-  actions: {
-    [key: string]: ActionObject<TContext, TEvent>;
-  };
   assign: <TEventType extends TEvent['type'] = TEvent['type']>(
     assigner:
       | Assigner<TContext, ExtractEvent<TEvent, TEventType>>
@@ -26,7 +22,6 @@ export function createModel<TContext, TEvent extends EventObject>(
 ): ContextModel<TContext, TEvent> {
   const model: ContextModel<TContext, TEvent> = {
     initialContext,
-    actions: {},
     assign
   };
 
