497c543d2980ea1a277b30b340a7bcd3dd0b3cb6
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-12-23 00:54:14 +0100

    Fixed an issue with events received from callback actors not having the appropriate `_event.origin` set

---
+++ .changeset/short-poets-sort.md
@@ -0,0 +1,5 @@
+---
+'xstate': patch
+---
+
+Fixed an issue with events received from callback actors not having the appropriate `_event.origin` set.
--- packages/core/test/event.test.ts
+++ packages/core/test/event.test.ts
@@ -2,7 +2,7 @@ import { Machine, sendParent, interpret, assign } from '../src';
 import { respond, send } from '../src/actions';
 
 describe('SCXML events', () => {
-  it('should have the origin (id) from the sending service', (done) => {
+  it('should have the origin (id) from the sending machine service', (done) => {
     const childMachine = Machine({
       initial: 'active',
       states: {
@@ -40,6 +40,36 @@ describe('SCXML events', () => {
       .start();
   });
 
+  it('should have the origin (id) from the sending callback service', () => {
+    const machine = Machine<{ childOrigin?: string }>({
+      initial: 'active',
+      context: {},
+      states: {
+        active: {
+          invoke: {
+            id: 'callback_child',
+            src: () => (send) => send({ type: 'EVENT' })
+          },
+          on: {
+            EVENT: {
+              target: 'success',
+              actions: assign({
+                childOrigin: (_, __, { _event }) => _event.origin
+              })
+            }
+          }
+        },
+        success: {
+          type: 'final'
+        }
+      }
+    });
+
+    const service = interpret(machine).start();
+
+    expect(service.state.context.childOrigin).toBe('callback_child');
+  });
+
   it('respond() should be able to respond to sender', (done) => {
     const authServerMachine = Machine({
       initial: 'waitingForCode',
@@ -47,7 +77,9 @@ describe('SCXML events', () => {
         waitingForCode: {
           on: {
             CODE: {
-              actions: respond('TOKEN', { delay: 10 })
+              actions: respond('TOKEN', {
+                delay: 10
+              })
             }
           }
         }
@@ -65,7 +97,9 @@ describe('SCXML events', () => {
             id: 'auth-server',
             src: authServerMachine
           },
-          entry: send('CODE', { to: 'auth-server' }),
+          entry: send('CODE', {
+            to: 'auth-server'
+          }),
           on: {
             TOKEN: 'authorized'
           }
