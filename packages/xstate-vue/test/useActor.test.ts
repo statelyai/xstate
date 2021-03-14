4cf5cdbf5fbbe2d98145a25d5a4b322aed34437e
Santi <scros97@gmail.com>
2021-03-14 20:59:35 +0100

    Add useActor tests

---
+++ packages/xstate-vue/test/useActor.test.ts
@@ -0,0 +1,34 @@
+import { render, fireEvent } from '@testing-library/vue';
+import UseActor from './UseActor.vue';
+import UseActorSimple from './UseActorSimple.vue';
+import UseActorCreateSimpleActor from './UseActorCreateSimpleActor.vue';
+
+describe('useActor composable function', () => {
+  it('initial invoked actor should be immediately available', async () => {
+    const { getByTestId } = render(UseActor);
+
+    const machineStateEl = getByTestId('machine-state');
+    const actorStateEl = getByTestId('actor-state');
+
+    expect(machineStateEl.textContent).toBe('active');
+    expect(actorStateEl.textContent).toBe('active');
+  });
+
+  it('actor should provide snapshot value immediately', () => {
+    const { getByTestId } = render(UseActorSimple);
+
+    const stateEl = getByTestId('state');
+    expect(stateEl.textContent).toEqual('42');
+  });
+
+  it('should update snapshot value when actor changes', async () => {
+    const { getByTestId } = render(UseActorCreateSimpleActor);
+
+    const stateEl = getByTestId('state');
+    const button = getByTestId('button');
+
+    expect(stateEl.textContent).toEqual('42');
+    await fireEvent.click(button);
+    expect(stateEl.textContent).toEqual('100');
+  });
+});
---
+++ packages/xstate-vue/test/useActorCreateSimpleActor.vue
@@ -0,0 +1,36 @@
+<template>
+  <div data-testid="state">{{ state }}</div>
+  <button data-testid="button" @click="actor = createSimpleActor(100)"></button>
+</template>
+
+<script lang="ts">
+import { ActorRef } from 'xstate';
+import { defineComponent, shallowRef } from 'vue';
+
+import { useActor } from '../src';
+
+const createSimpleActor = (
+  value: number
+): ActorRef<any, number> & { latestValue: number } => ({
+  send: () => {
+    /* ... */
+  },
+  latestValue: value,
+  subscribe: () => {
+    return {
+      unsubscribe: () => {
+        /* ... */
+      }
+    };
+  }
+});
+
+export default defineComponent({
+  setup() {
+    const actor = shallowRef(createSimpleActor(42));
+    const { state } = useActor(actor, (a) => a.latestValue);
+
+    return { actor, state, createSimpleActor };
+  }
+});
+</script>
