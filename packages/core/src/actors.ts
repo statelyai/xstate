0d768c09894545a6e8a92708e85bc12f0dcd5c50
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-03-17 21:47:16 +0100

    Upgrade Preconstruct, some build-related cleanups, stub of new actors entrypoint

---
+++ packages/core/src/actors.ts
@@ -0,0 +1,15 @@
+export function fromDeferred() {}
+export function fromMachine() {}
+export function fromObservable() {}
+export function fromPromise() {}
+
+export function spawn() {}
+
+export function spawnDeferred() {}
+export function spawnMachine() {}
+export function spawnObservable() {}
+export function spawnPromise() {}
+
+export function from() {}
+
+export function spawnFrom() {}
