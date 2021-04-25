2938b17380e0b573910b822f0998535992d859e4
David Khourshid <davidkpiano@gmail.com>
2021-04-25 11:30:51 -0400

    Add test for parallel states

--- packages/core/test/tags.test.ts
+++ packages/core/test/tags.test.ts
@@ -52,6 +52,45 @@ describe('tags', () => {
     expect(machine.initialState.tags.has('crosswalkLight')).toBeTruthy();
   });
 
+  it('supports tags in parallel states', () => {
+    const machine = createMachine({
+      type: 'parallel',
+      states: {
+        foo: {
+          initial: 'active',
+          states: {
+            active: {
+              tags: 'yes'
+            },
+            inactive: {
+              tags: 'no'
+            }
+          }
+        },
+        bar: {
+          initial: 'active',
+          states: {
+            active: {
+              tags: 'yes',
+              on: {
+                DEACTIVATE: 'inactive'
+              }
+            },
+            inactive: {
+              tags: 'no'
+            }
+          }
+        }
+      }
+    });
+
+    let state = machine.initialState;
+
+    expect(state.tags).toEqual(new Set(['yes']));
+    state = machine.transition(state, 'DEACTIVATE');
+    expect(state.tags).toEqual(new Set(['yes', 'no']));
+  });
+
   it('tags can be single (not array)', () => {
     const machine = createMachine({
       initial: 'green',
