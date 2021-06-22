a6c3324cfe623c6cf75cba655d25b38fe617bc7e
David Khourshid <davidkpiano@gmail.com>
2021-06-22 12:42:03 -0400

    Fix wrong import

--- packages/xstate-react/src/useSpawn.ts
+++ packages/xstate-react/src/useSpawn.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { ActorRef, Behavior, EventObject } from 'xstate';
-import { spawnBehavior } from 'xstate/src/behavior';
+import { spawnBehavior } from 'xstate/src/behaviors';
 
 /**
  * React hook that spawns an `ActorRef` with the specified `behavior`.
