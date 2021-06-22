30819279794a56efceee395aec8a4025e7ba2278
David Khourshid <davidkpiano@gmail.com>
2021-06-22 09:12:28 -0400

    Add TypeScript

--- blog/docusaurus.config.js
+++ blog/docusaurus.config.js
@@ -1,17 +1,17 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'XState Posts',
+  tagline: 'Posts about XState',
+  url: 'https://xstate.js.org/posts',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'statelyai', // Usually your GitHub org/user name.
+  projectName: 'xstate', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'XState',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg'
