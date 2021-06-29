8b4170b8aee8679c3e694d3e49f497068a0a43f4
Wouter Termont <woutermont@gmail.com>
2021-06-29 15:29:31 +0200

    chore: correct merge fault
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- packages/core/src/interpreter.ts
+++ packages/core/src/interpreter.ts
@@ -1282,12 +1282,6 @@ export class Interpreter<
     return this;
   }
 
-  public [Symbol.observable](): Subscribable<
-    State<TContext, TEvent, TStateSchema, TTypestate>
-  > {
-    return this;
-  }
-
   public getSnapshot() {
     if (this.status === InterpreterStatus.NotStarted) {
       return this.initialState;
