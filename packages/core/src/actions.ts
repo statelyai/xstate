1a978a034c7c11317e84ab43791c1acfa9c1c2ed
David Khourshid <davidkpiano@gmail.com>
2021-06-30 23:04:33 -0400

    Will do raise() improvements in separate PR

--- packages/core/src/actions.ts
+++ packages/core/src/actions.ts
@@ -159,12 +159,6 @@ export function toActivityDefinition<TContext, TEvent extends EventObject>(
  *
  * @param eventType The event to raise.
  */
-export function raise<TEvent extends EventObject>(
-  event: Event<TEvent>
-): RaiseAction<TEvent> | SendAction<any, AnyEventObject, TEvent>;
-export function raise<TContext, TEvent extends EventObject>(
-  event: Event<TEvent>
-): RaiseAction<TEvent> | SendAction<TContext, AnyEventObject, TEvent>;
 export function raise<TContext, TEvent extends EventObject>(
   event: Event<TEvent>
 ): RaiseAction<TEvent> | SendAction<TContext, AnyEventObject, TEvent> {
