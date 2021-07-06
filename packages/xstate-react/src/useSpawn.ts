0cbc67334953aaa3d255ebf15502079c68f9e427
David Khourshid <davidkpiano@gmail.com>
2021-07-06 13:29:21 -0400

    Change import of spawnBehavior

--- packages/xstate-react/src/useSpawn.ts
+++ packages/xstate-react/src/useSpawn.ts
@@ -1,5 +1,5 @@
 import { ActorRef, Behavior, EventObject } from 'xstate';
-import { spawnBehavior } from 'xstate/src/behaviors';
+import { spawnBehavior } from 'xstate/lib/behaviors';
 import useConstant from './useConstant';
 
 /**
