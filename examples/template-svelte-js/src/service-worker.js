75d3c79e46204f6e9a162933a98a0303e20c3a12
Anna Bansaghi <anna.bansaghi@gmail.com>
2021-07-20 09:04:31 +0200

    Format with local Prettier

--- examples/template-svelte-js/src/service-worker.js
+++ examples/template-svelte-js/src/service-worker.js
@@ -1,45 +1,51 @@
 const CACHE_NAME = `cache_${TIMESTAMP}`;
 
-const shell = ['/', '/manifest.json', '/global.css', '/build/bundle.js', '/build/bundle.css'];
+const shell = [
+  '/',
+  '/manifest.json',
+  '/global.css',
+  '/build/bundle.js',
+  '/build/bundle.css'
+];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then((cache) => cache.addAll(shell))
-            .then(() => {
-                self.skipWaiting();
-            })
-    );
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(shell))
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then(async (keys) => {
-            // delete old caches
-            for (const key of keys) {
-                if (key !== CACHE_NAME) await caches.delete(key);
-            }
+  event.waitUntil(
+    caches.keys().then(async (keys) => {
+      // delete old caches
+      for (const key of keys) {
+        if (key !== CACHE_NAME) await caches.delete(key);
+      }
 
-            self.clients.claim();
-        })
-    );
+      self.clients.claim();
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    // try the network first, falling back to cache if offline
-    event.respondWith(
-        caches.open(CACHE_NAME).then(async (cache) => {
-            try {
-                const response = await fetch(event.request);
-                cache.put(event.request, response.clone());
-                return response;
-            } catch (err) {
-                const response = await cache.match(event.request);
-                if (response) return response;
+  // try the network first, falling back to cache if offline
+  event.respondWith(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      try {
+        const response = await fetch(event.request);
+        cache.put(event.request, response.clone());
+        return response;
+      } catch (err) {
+        const response = await cache.match(event.request);
+        if (response) return response;
 
-                throw err;
-            }
-        })
-    );
+        throw err;
+      }
+    })
+  );
 });
