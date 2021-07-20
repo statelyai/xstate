5bbfdb68b6f652be01a5e418bc7078f88195faa6
Anna Bansaghi <anna.bansaghi@gmail.com>
2021-07-20 14:52:51 +0200

    Make the template smaller

--- examples/template-svelte-js/rollup.config.js
+++ examples/template-svelte-js/rollup.config.js
@@ -33,68 +33,53 @@ function serve() {
   };
 }
 
-export default [
-  {
-    input: 'src/main.js',
-    output: {
-      sourcemap: true,
-      format: 'iife',
-      name: 'app',
-      file: 'public/build/bundle.js'
-    },
-    plugins: [
-      replace({
-        preventAssignment: true,
-        'process.env.NODE_ENV': process.env.NODE_ENV
-      }),
-      svelte({
-        compilerOptions: {
-          // enable run-time checks when not in production
-          dev: !production
-        }
-      }),
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css({ output: 'bundle.css' }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration -
-      // consult the documentation for details:
-      // https://github.com/rollup/plugins/tree/master/packages/commonjs
-      resolve({
-        browser: true,
-        dedupe: ['svelte']
-      }),
-      commonjs(),
-
-      // In dev mode, call `npm run start` once
-      // the bundle has been generated
-      !production && serve(),
-
-      // Watch the `public` directory and refresh the
-      // browser on changes when not in production
-      !production && livereload('public'),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      production && terser()
-    ],
-    watch: {
-      clearScreen: false
-    }
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
   },
-  {
-    input: 'src/service-worker.js',
-    output: {
-      file: 'public/service-worker.js',
-      format: 'esm'
-    },
-    plugins: [
-      replace({
-        preventAssignment: true,
-        TIMESTAMP: Date.now()
-      })
-    ]
+  plugins: [
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': process.env.NODE_ENV
+    }),
+    svelte({
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production
+      }
+    }),
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css({ output: 'bundle.css' }),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
+
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload('public'),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
   }
-];
+};
--- examples/template-svelte-js/src/main.js
+++ examples/template-svelte-js/src/main.js
@@ -4,8 +4,4 @@ const app = new App({
   target: document.body
 });
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
-}
-
 export default app;
