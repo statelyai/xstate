2e94de5ec2bc03f5ef53987c273947d53d3e418e
chanced <chanceusc@gmail.com>
2021-08-24 07:58:17 -0400

    Revert "renames actionTypes to screaming snake case so not to be confused with actions"
    This reverts commit 7e597dedf0f66a1571f51b943f409ea18f22d197.

--- packages/core/src/actionTypes.ts
+++ packages/core/src/actionTypes.ts
@@ -1,23 +1,21 @@
 import { ActionTypes } from './types';
 
 // xstate-specific action types
-export const XSTATE_START = ActionTypes.Start;
-export const XSTATE_STOP = ActionTypes.Stop;
-export const XSTATE_RAISE = ActionTypes.Raise;
-export const XSTATE_SEND = ActionTypes.Send;
-export const XSTATE_CANCEL = ActionTypes.Cancel;
-export const XSTATE_ASSIGN = ActionTypes.Assign;
-export const XSTATE_AFTER = ActionTypes.After;
-export const XSTATE_LOG = ActionTypes.Log;
-export const XSTATE_INIT = ActionTypes.Init;
-export const XSTATE_INVOKE = ActionTypes.Invoke;
-export const XSTATE_ERROR = ActionTypes.ErrorCustom;
-export const XSTATE_UPDATE = ActionTypes.Update;
-export const XSTATE_CHOOSE = ActionTypes.Choose;
-export const XSTATE_PURE = ActionTypes.Pure;
-
-export const NULL_EVENT = ActionTypes.NullEvent;
-export const DONE_START = ActionTypes.DoneState;
-
-export const ERROR_EXECUTION = ActionTypes.ErrorExecution;
-export const ERROR_PLATFORM = ActionTypes.ErrorPlatform;
+export const start = ActionTypes.Start;
+export const stop = ActionTypes.Stop;
+export const raise = ActionTypes.Raise;
+export const send = ActionTypes.Send;
+export const cancel = ActionTypes.Cancel;
+export const nullEvent = ActionTypes.NullEvent;
+export const assign = ActionTypes.Assign;
+export const after = ActionTypes.After;
+export const doneState = ActionTypes.DoneState;
+export const log = ActionTypes.Log;
+export const init = ActionTypes.Init;
+export const invoke = ActionTypes.Invoke;
+export const errorExecution = ActionTypes.ErrorExecution;
+export const errorPlatform = ActionTypes.ErrorPlatform;
+export const error = ActionTypes.ErrorCustom;
+export const update = ActionTypes.Update;
+export const choose = ActionTypes.Choose;
+export const pure = ActionTypes.Pure;
