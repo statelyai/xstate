de7344c146d49e9399295ce799187627262aa8cc
Matt Pocock <mattpocockvoice@gmail.com>
2021-08-30 10:04:22 +0100

    Removed unused babel preset

--- packages/xstate-parser/babel.config.js
+++ packages/xstate-parser/babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: '12' } }],
-    '@babel/preset-typescript',
-    '@babel/preset-react'
+    '@babel/preset-typescript'
   ]
 };
