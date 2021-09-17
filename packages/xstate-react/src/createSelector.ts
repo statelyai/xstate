063e04e0155c831446ef06a3e7e8ede7bc9fffdd
Matt Pocock <mattpocockvoice@gmail.com>
2021-09-17 09:57:55 +0100

    Update packages/xstate-react/src/createSelector.ts
    Co-authored-by: Corentin Leruth <corentin.leruth@gmail.com>

--- packages/xstate-react/src/createSelector.ts
+++ packages/xstate-react/src/createSelector.ts
@@ -4,7 +4,7 @@ import { StateFrom, StateMachine } from 'xstate';
  * Allows you to specify a typesafe selector based on a machine definition.
  *
  * @example
- * const getIsToggedOn = createSelector(machine, state => state.matches('toggledOn'));
+ * const getIsToggledOn = createSelector(machine, state => state.matches('toggledOn'));
  */
 export const createSelector = <TMachine extends StateMachine<any, any, any>, T>(
   _machine: TMachine,
