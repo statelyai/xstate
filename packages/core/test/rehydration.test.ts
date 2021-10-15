707cb981fdb8a5c75cacb7e9bfa5c7e5a1cc1c88
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-10-15 16:39:51 +0200

    Fixed an issue with tags being missed on a service state after starting that service using a state value

--- packages/core/test/rehydration.test.ts
+++ packages/core/test/rehydration.test.ts
@@ -1,7 +1,7 @@
 import { createMachine, interpret, State } from '../src';
 
 describe('rehydration', () => {
-  it('should be able to use `hasTag` on the rehydrated state', () => {
+  it('should be able to use `hasTag` after rehydrating persisted state', () => {
     const machine = createMachine({
       initial: 'a',
       states: {
@@ -15,6 +15,26 @@ describe('rehydration', () => {
     const restoredState = State.create(JSON.parse(persistedState));
 
     const service = interpret(machine).start(restoredState);
+
+    expect(service.state.hasTag('foo')).toBe(true);
+  });
+
+  it('should be able to use `hasTag` after rehydrating state value', () => {
+    const machine = createMachine({
+      initial: 'inactive',
+      states: {
+        inactive: {
+          on: { NEXT: 'active' }
+        },
+        active: {
+          tags: 'foo'
+        }
+      }
+    });
+
+    const service = interpret(machine);
+
+    service.start('active');
 
     expect(service.state.hasTag('foo')).toBe(true);
   });
