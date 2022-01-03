37547c356cf5f6fe5e073c9c5e1df57e4ddcb43b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-01-03 15:59:10 +0100

    Refactor Mailbox to use a basic linked list

--- packages/core/src/Mailbox.ts
+++ packages/core/src/Mailbox.ts
@@ -1,32 +1,56 @@
-export class Mailbox<T> {
-  private events: T[] = [];
-  private index: number = 0;
+interface MailboxItem<T> {
+  value: T;
+  next: MailboxItem<T> | null;
+}
 
-  public status: 'deferred' | 'idle' | 'processing' = 'deferred';
+export class Mailbox<T> {
+  private _active: boolean = false;
+  private _current: MailboxItem<T> | null = null;
+  private _last: MailboxItem<T> | null = null;
+
+  constructor(private _process: (ev: T) => void) {}
+
+  public start() {
+    this._active = true;
+    this.flush();
+  }
 
   public clear(): void {
-    this.events.length = 0;
-    this.index = 0;
+    // we can't set _current to null because we might be currently processing
+    // and enqueue following clear shouldnt start processing the enqueued item immediately
+    if (this._current) {
+      this._current.next = null;
+      this._last = this._current;
+    }
   }
 
   public enqueue(event: T): void {
-    this.events.push(event);
+    const enqueued = {
+      value: event,
+      next: null
+    };
+
+    if (this._current) {
+      this._last!.next = enqueued;
+      this._last = enqueued;
+      return;
+    }
+
+    this._current = enqueued;
+    this._last = enqueued;
+
+    if (this._active) {
+      this.flush();
+    }
   }
 
-  public dequeue(): T | undefined {
-    const event = this.events[this.index];
-
-    if (!event) {
-      return undefined;
+  private flush() {
+    while (this._current) {
+      // atm the given _process is responsible for implementing proper try/catch handling
+      // we assume here that this won't throw in a way that can affect this mailbox
+      this._process(this._current.value);
+      this._current = this._current.next;
     }
-
-    this.index++;
-
-    if (this.index > this.events.length - 1) {
-      this.events.length = 0;
-      this.index = 0;
-    }
-
-    return event;
+    this._last = null;
   }
 }
