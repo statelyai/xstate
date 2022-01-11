fa84c142a2755749839fdfd5dbba2fc9fca5959b
GoldingAustin <16907830+GoldingAustin@users.noreply.github.com>
2022-01-10 18:03:08 -0800

    Remove rootDir Jest config and include separate babel.config.js

--- packages/xstate-solid/jest.config.js
+++ packages/xstate-solid/jest.config.js
@@ -3,9 +3,8 @@ const path = require('path');
 
 module.exports = {
   preset: 'solid-jest/preset/browser',
-  rootDir: '.',
   transform: {
-    '^.+\\.(js|ts|tsx)?$': path.resolve(__dirname, './jestTransform.js')
+    '^.+\\.(js|ts|tsx)?$': path.resolve(__dirname, 'jestTransform.js')
   },
   moduleNameMapper: lernaAliases()
 };
