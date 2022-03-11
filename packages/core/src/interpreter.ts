4cf89b5f9cf645f741164d23e3bc35dd7c5706f6
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-03-11 16:44:49 +0100

    Fixed compatibility of `Interpreter` with older versions of TypeScript (#3146)

--- packages/core/src/interpreter.ts
+++ packages/core/src/interpreter.ts
@@ -112,7 +112,6 @@ export enum InterpreterStatus {
   Stopped
 }
 
-/** @ts-ignore [symbolObservable] creates problems for people without `skipLibCheck` who are on older versions of TS, remove this comment when we drop support for TS@<4.3 */
 export class Interpreter<
   // tslint:disable-next-line:max-classes-per-file
   TContext,
@@ -1372,7 +1371,6 @@ export class Interpreter<
     };
   }
 
-  /** @ts-ignore this creates problems for people without `skipLibCheck` who are on older versions of TS, remove this comment when we drop support for TS@<4.3 */
   public [symbolObservable](): InteropSubscribable<
     State<TContext, TEvent, TStateSchema, TTypestate, TResolvedTypesMeta>
   > {
