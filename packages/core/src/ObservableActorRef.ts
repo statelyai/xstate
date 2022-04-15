664665a0f59232a0d353ab06f0d662dcd1052464
David Khourshid <davidkpiano@gmail.com>
2022-04-14 22:18:18 -0400

    Remove Actor class

--- packages/core/src/ObservableActorRef.ts
+++ packages/core/src/ObservableActorRef.ts
@@ -1,16 +1,21 @@
 import { EventObject, ActorRef, Behavior, SCXML, ActorContext } from './types';
-import { Actor } from './actor';
 import { symbolObservable, toSCXMLEvent } from './utils';
+import { Mailbox } from './Mailbox';
+import { LifecycleSignal, startSignal, stopSignal } from './actors';
+
+const nullSubscription = {
+  unsubscribe: () => void 0
+};
 
 export class ObservableActorRef<TEvent extends EventObject, TEmitted>
   implements ActorRef<TEvent, TEmitted> {
   private current: TEmitted;
   public deferred = true;
   private context: ActorContext<TEvent, TEmitted>;
-  private actor: Actor<TEvent, TEmitted>;
   public name: string;
+  private mailbox = new Mailbox(this._process.bind(this));
 
-  constructor(behavior: Behavior<TEvent, TEmitted>, name: string) {
+  constructor(public behavior: Behavior<TEvent, TEmitted>, name: string) {
     this.name = name;
     this.context = {
       self: this,
@@ -18,24 +23,25 @@ export class ObservableActorRef<TEvent extends EventObject, TEmitted>
       observers: new Set(),
       _event: toSCXMLEvent({ type: 'xstate.init' }) as SCXML.Event<TEvent> // TODO: fix
     };
-    this.actor = new Actor(behavior, name, this.context);
-    this.current = this.actor.current;
+    this.current = this.behavior.initialState;
   }
   public start() {
     this.deferred = false;
-    this.actor.start();
+    this.mailbox.enqueue(startSignal);
+    this.mailbox.start();
 
     return this;
   }
   public stop() {
-    this.actor.stop();
+    this.mailbox.clear();
+    this.mailbox.enqueue(stopSignal);
     return this;
   }
   public subscribe(observer) {
-    return this.actor.subscribe(observer);
+    return this.behavior.subscribe?.(observer) ?? nullSubscription;
   }
   public send(event) {
-    this.actor.receive(event);
+    this.receive(event);
   }
   public toJSON() {
     return {
@@ -44,9 +50,26 @@ export class ObservableActorRef<TEvent extends EventObject, TEmitted>
     };
   }
   public getSnapshot() {
-    return this.actor.current;
+    return this.current;
   }
   public [symbolObservable]() {
     return this;
+  }
+  public receive(event: TEvent | LifecycleSignal) {
+    this.mailbox.enqueue(event);
+  }
+  private _process(event: TEvent | LifecycleSignal) {
+    this.context._event =
+      typeof event.type !== 'string'
+        ? (event as LifecycleSignal)
+        : toSCXMLEvent(event as TEvent);
+
+    this.current = this.behavior.transition(
+      this.current,
+      typeof this.context._event.type !== 'string'
+        ? (this.context._event as LifecycleSignal)
+        : (this.context._event as SCXML.Event<TEvent>).data,
+      this.context
+    );
   }
 }
--- packages/core/src/actor.ts
+++ packages/core/src/actor.ts
@@ -1,81 +1,8 @@
-import {
-  EventObject,
-  SCXML,
-  ActorRef,
-  BaseActorRef,
-  Behavior,
-  ActorContext
-} from './types';
-import { LifecycleSignal, startSignal, stopSignal } from './actors';
-import { symbolObservable, toSCXMLEvent } from './utils';
-import { Mailbox } from './Mailbox';
-
-const nullSubscription = {
-  unsubscribe: () => void 0
-};
+import { EventObject, ActorRef, BaseActorRef } from './types';
+import { symbolObservable } from './utils';
 
 export function isActorRef(item: any): item is ActorRef<any> {
   return !!item && typeof item === 'object' && typeof item.send === 'function';
-}
-
-export function isSpawnedActorRef(item: any): item is ActorRef<any> {
-  return isActorRef(item) && 'name' in item;
-}
-
-export class Actor<TEvent extends EventObject, TEmitted> {
-  public current: TEmitted;
-  private context: ActorContext<TEvent, TEmitted>;
-  private behavior: Behavior<TEvent, TEmitted>;
-  private mailbox: Mailbox<TEvent | LifecycleSignal> = new Mailbox(
-    this._process.bind(this)
-  );
-
-  public name: string;
-
-  constructor(
-    behavior: Behavior<TEvent, TEmitted>,
-    name: string,
-    actorContext: ActorContext<TEvent, TEmitted>
-  ) {
-    this.behavior = behavior;
-    this.name = name;
-    this.context = actorContext;
-    this.current = behavior.initialState;
-    this.mailbox.enqueue(startSignal);
-  }
-  public start() {
-    this.mailbox.start();
-    return this;
-  }
-  public stop() {
-    // TODO: test this behavior
-    this.mailbox.clear();
-    this.mailbox.enqueue(stopSignal);
-  }
-  public subscribe(observer) {
-    return this.behavior.subscribe?.(observer) || nullSubscription;
-  }
-  public receive(event: TEvent | LifecycleSignal) {
-    this.mailbox.enqueue(event);
-  }
-  private _process(event: TEvent | LifecycleSignal) {
-    this.context._event =
-      typeof event.type !== 'string'
-        ? (event as LifecycleSignal)
-        : toSCXMLEvent(event as TEvent);
-
-    this.current = this.behavior.transition(
-      this.current,
-      typeof this.context._event.type !== 'string'
-        ? (this.context._event as LifecycleSignal)
-        : (this.context._event as SCXML.Event<TEvent>).data,
-      this.context
-    );
-  }
-}
-
-export function isSpawnedActor(item: any): item is ActorRef<any> {
-  return isActorRef(item) && 'id' in item;
 }
 
 // TODO: refactor the return type, this could be written in a better way but it's best to avoid unneccessary breaking changes now
