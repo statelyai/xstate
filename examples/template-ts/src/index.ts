df893ce5fd26d04ce1024b807e2cc8091b01ad4c
David Khourshid <davidkpiano@gmail.com>
2022-04-29 15:10:51 -0400

    Improve example

--- examples/template-ts/src/index.ts
+++ examples/template-ts/src/index.ts
@@ -5,51 +5,63 @@ const eventOutputEl = document.getElementById('output-event')!;
 const valueOutputEl = document.getElementById('output-value')!;
 const contextOutputEl = document.getElementById('output-context')!;
 
-const taskMachine = createMachine<{ time?: string }>({
-  initial: 'idle',
-  context: {
-    time: undefined
-  },
-  states: {
-    idle: {
-      on: {
-        fetch: 'pending'
-      }
+const timeMachine = createMachine<{ time?: string }>(
+  {
+    id: 'time',
+    initial: 'idle',
+    context: {
+      time: undefined
     },
-    pending: {
-      invoke: {
-        src: () =>
-          new Promise((res) => {
-            setTimeout(() => {
-              res(new Date().toLocaleTimeString());
-            }, 1000);
-          }),
-        onDone: {
-          actions: assign({
-            time: (_, event) => event.data
-          })
+    states: {
+      idle: {
+        on: {
+          fetch: 'pending'
+        }
+      },
+      pending: {
+        invoke: {
+          src: 'fetchTime',
+          onDone: {
+            actions: assign({
+              time: (_, event) => event.data
+            })
+          }
+        },
+        on: {
+          cancel: 'idle'
+        }
+      },
+      success: {
+        on: {
+          refetch: 'pending'
         }
       }
-    },
-    success: {
-      on: {
-        refetch: 'pending'
-      }
+    }
+  },
+  {
+    services: {
+      fetchTime: () =>
+        new Promise((res) => {
+          setTimeout(() => {
+            res(new Date().toLocaleTimeString());
+          }, 1000);
+        })
     }
   }
-});
+);
 
-function renderState(state: StateFrom<typeof taskMachine>) {
+function renderState(state: StateFrom<typeof timeMachine>) {
   console.log(state);
   eventOutputEl.innerHTML = JSON.stringify(state.event, null, 2);
   valueOutputEl.innerHTML = JSON.stringify(state.value, null, 2);
   contextOutputEl.innerHTML = JSON.stringify(state.context, null, 2);
+  console.log(`Next possible events:  ${state.nextEvents.join(', ')}`);
 }
 
-const time = interpret(taskMachine)
+const timeActor = interpret(timeMachine)
   .onTransition((state) => {
     renderState(state);
   })
   .start();
 
-(window as any).time = time;
+(window as any).timeActor = timeActor;
