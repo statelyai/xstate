0dfc6d92f6950b3eb78e0693ae3b0abe5751bf42
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-08-05 14:27:43 +0200

    Fixed an issue with `useSelector` always computing fresh snapshots internally for uninitialized services (#3500)
    * Fixed an issue with `useSelector` always computing fresh snapshots internally for uninitialized services
    
    * Update packages/xstate-react/src/useSelector.ts
    
    Co-authored-by: David Khourshid <davidkpiano@gmail.com>
    
    Co-authored-by: David Khourshid <davidkpiano@gmail.com>

--- packages/xstate-react/src/useSelector.ts
+++ packages/xstate-react/src/useSelector.ts
@@ -1,6 +1,6 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/shim/with-selector';
-import { ActorRef, Interpreter, Subscribable } from 'xstate';
+import { ActorRef, AnyState, Interpreter, Subscribable } from 'xstate';
 import { isActorWithState } from './useActor';
 import { getServiceSnapshot } from './utils';
 
@@ -9,12 +9,18 @@ function isService(actor: any): actor is Interpreter<any, any, any, any> {
 }
 
 const defaultCompare = (a, b) => a === b;
-const defaultGetSnapshot = (a) =>
-  isService(a)
-    ? getServiceSnapshot(a)
-    : isActorWithState(a)
-    ? a.state
-    : undefined;
+const defaultGetSnapshot = (a, initialStateCacheRef) => {
+  if (isService(a)) {
+    // A status of 0 = interpreter not started
+    if (a.status === 0 && initialStateCacheRef.current) {
+      return initialStateCacheRef.current;
+    }
+    const snapshot = getServiceSnapshot(a);
+    initialStateCacheRef.current = a.status === 0 ? snapshot : null;
+    return snapshot;
+  }
+  return isActorWithState(a) ? a.state : undefined;
+};
 
 export function useSelector<
   TActor extends ActorRef<any, any>,
@@ -24,8 +30,10 @@ export function useSelector<
   actor: TActor,
   selector: (emitted: TEmitted) => T,
   compare: (a: T, b: T) => boolean = defaultCompare,
-  getSnapshot: (a: TActor) => TEmitted = defaultGetSnapshot
+  getSnapshot?: (a: TActor) => TEmitted
 ): T {
+  const initialStateCacheRef = useRef<AnyState | null>(null);
+
   const subscribe = useCallback(
     (handleStoreChange) => {
       const { unsubscribe } = actor.subscribe(handleStoreChange);
@@ -34,10 +42,12 @@ export function useSelector<
     [actor]
   );
 
-  const boundGetSnapshot = useCallback(() => getSnapshot(actor), [
-    actor,
-    getSnapshot
-  ]);
+  const boundGetSnapshot = useCallback(() => {
+    if (getSnapshot) {
+      return getSnapshot(actor);
+    }
+    return defaultGetSnapshot(actor, initialStateCacheRef);
+  }, [actor, getSnapshot]);
 
   const selectedSnapshot = useSyncExternalStoreWithSelector(
     subscribe,
