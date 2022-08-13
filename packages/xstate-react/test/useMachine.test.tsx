6ab24c2d245f3ef48ba0fcff6f44376f97d27caf
David Khourshid <davidkpiano@gmail.com>
2022-08-13 13:46:14 -0400

    Fixing tests WIP

--- packages/xstate-react/test/useMachine.test.tsx
+++ packages/xstate-react/test/useMachine.test.tsx
@@ -376,14 +376,7 @@ describeEachReactMode('useMachine (%s)', ({ suiteKey, render }) => {
 
     render(<App />);
 
-    expect(rerenders).toBe(
-      suiteKey === 'strict'
-        ? // it's rendered twice for the each state
-          // and the machine gets currently completely restarted in a double-invoked strict effect
-          // so we get a new state from that restarted machine (and thus 2 additional strict renders) and we end up with 4
-          4
-        : 1
-    );
+    expect(rerenders).toBe(suiteKey === 'strict' ? 2 : 1);
   });
 
   it('should maintain the same reference for objects created when resolving initial state', () => {
@@ -434,20 +427,12 @@ describeEachReactMode('useMachine (%s)', ({ suiteKey, render }) => {
 
     const { getByRole } = render(<App />);
 
-    expect(effectsFired).toBe(
-      suiteKey === 'strict'
-        ? // TODO: probably it should be 2 for strict mode cause of the double-invoked strict effects
-          // atm it's 3 cause we the double-invoked effect sees the initial value
-          // but the 3rd call comes from the restarted machine (that happens because of the strict effects)
-          // the second effect with `service.start()` doesn't have a way to change what another effect in the same "effect batch" sees
-          3
-        : 1
-    );
+    expect(effectsFired).toBe(suiteKey === 'strict' ? 2 : 1);
 
     const button = getByRole('button');
     fireEvent.click(button);
 
-    expect(effectsFired).toBe(suiteKey === 'strict' ? 3 : 1);
+    expect(effectsFired).toBe(suiteKey === 'strict' ? 2 : 1);
   });
 
   it('should successfully spawn actors from the lazily declared context', () => {
@@ -706,7 +691,7 @@ describeEachReactMode('useMachine (%s)', ({ suiteKey, render }) => {
 
     render(<Test />);
 
-    expect(activatedCount).toEqual(suiteKey === 'strict' ? 2 : 1);
+    expect(activatedCount).toEqual(suiteKey === 'strict' ? 1 : 1);
   });
 
   it('child component should be able to send an event to a parent immediately in an effect', (done) => {
@@ -857,7 +842,10 @@ describeEachReactMode('useMachine (%s)', ({ suiteKey, render }) => {
       entry: [assign({ count: 1 }), send('INC')],
       on: {
         INC: {
-          actions: [assign({ count: (ctx) => ++ctx.count }), send('UNHANDLED')]
+          actions: [
+            assign({ count: (ctx) => ctx.count + 1 }),
+            send('UNHANDLED')
+          ]
         }
       },
       states: {
