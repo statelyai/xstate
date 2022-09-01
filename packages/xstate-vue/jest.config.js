c765953bf93612dc814b887bc5dcd301ef55b6f7
with-heart <with.heart+git@pm.me>
2022-09-01 14:06:31 -0400

    set vue3-jest to read test/tsconfig.json

--- packages/xstate-vue/jest.config.js
+++ packages/xstate-vue/jest.config.js
@@ -1,17 +1,21 @@
+const path = require('path');
 const { jest: lernaAliases } = require('lerna-alias');
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   moduleNameMapper: lernaAliases(),
-  moduleFileExtensions: ['js', 'vue'],
   transform: {
-    '^.+\\.js$': 'babel-jest',
     // process *.vue files with vue-jest
     '^.+\\.vue$': require.resolve('@vue/vue3-jest')
   },
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
     customExportConditions: ['node', 'node-addons']
+  },
+  globals: {
+    'vue-jest': {
+      tsConfig: path.join(__dirname, 'test', 'tsconfig.json')
+    }
   }
 };
