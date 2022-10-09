fed527576c09378ffd8ae1a4dfeae9e4f2a7b255
David Khourshid <davidkpiano@gmail.com>
2022-10-09 16:25:19 -0400

    Fix types

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1890,7 +1890,7 @@ export type AnyBehavior = Behavior<any, any, any>;
 
 export type SnapshotFrom<T> = ReturnTypeOrValue<T> extends infer R
   ? R extends Interpreter<infer _, infer __>
-    ? R['initialState']
+    ? ReturnType<R['getInitialState']>
     : R extends ActorRef<infer _, infer TSnapshot>
     ? TSnapshot
     : R extends Behavior<infer _, infer TSnapshot>
