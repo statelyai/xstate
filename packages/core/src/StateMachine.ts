ec4a4c185bbb3ceccd8004d2930a83e0074e845e
David Khourshid <davidkpiano@gmail.com>
2022-12-17 04:26:26 -0700

    Update packages/core/src/StateMachine.ts
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/core/src/StateMachine.ts
+++ packages/core/src/StateMachine.ts
@@ -301,7 +301,6 @@ export class StateMachine<
     state: State<TContext, TEvent, TResolvedTypesMeta>,
     _event: SCXML.Event<TEvent>
   ): Array<TransitionDefinition<TContext, TEvent>> {
-    // return this.transition(state, _event).transitions;
     return transitionNode(this.root, state.value, state, _event) || [];
   }
 
