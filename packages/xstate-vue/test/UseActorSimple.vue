9080ed102aeb6b860caf6d6118ddc8809c9446aa
David Khourshid <davidkpiano@gmail.com>
2022-12-19 13:20:28 -0500

    Update packages/xstate-vue/test/UseActorSimple.vue
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/xstate-vue/test/UseActorSimple.vue
+++ packages/xstate-vue/test/UseActorSimple.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { ActorRef } from 'xstate';
+import { interpret, ActorRef } from 'xstate';
 import { interpret } from 'xstate/src';
 import { useActor } from '../src';
 const simpleActor: ActorRef<any, number> = interpret({
