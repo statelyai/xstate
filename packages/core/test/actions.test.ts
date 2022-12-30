daf5f4e1600864e344bcf87b294723b92e40c6fc
David Khourshid <davidkpiano@gmail.com>
2022-12-30 15:14:14 -0500

    Update packages/core/test/actions.test.ts
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/core/test/actions.test.ts
+++ packages/core/test/actions.test.ts
@@ -1486,7 +1486,6 @@ describe('initial actions', () => {
   });
 
   it('should support initial actions', () => {
-    // TODO: fix initial state actions on root node
     expect(machine.initialState.actions.map((a) => a.type)).toEqual([
       'initialA',
       'entryA'
