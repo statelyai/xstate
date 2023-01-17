0c1b210ca37a7d90ab5fbdc04b9d9c996addefb2
David Khourshid <davidkpiano@gmail.com>
2023-01-17 10:16:30 -0500

    Fix comment

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -600,7 +600,9 @@ export interface StateNodeConfig<
    */
   states?: StatesConfig<TContext, TEvent, TAction> | undefined;
   /**
-   * The actors to invoke upon entering this state node. These actors will be stopped upon exiting this state node.
+   * The behaviors to invoke upon entering this state node.
+   * An invoked behavior is an actor.
+   * These actors will be stopped upon exiting this state node.
    */
   invoke?: SingleOrArray<
     string | BehaviorCreator<TContext, TEvent> | InvokeConfig<TContext, TEvent>
