ef347468e14666792a954d228c1d0b16e5fa11df
Aldo Velasco <aldovelco@gmail.com>
2023-01-26 10:00:17 -0600

    Use standalone components

--- examples/template-angular-ts/src/app/app.component.ts
+++ examples/template-angular-ts/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { from } from 'rxjs';
 import { toggleMachine } from 'src/toggle.machine';
 import { interpret } from 'xstate';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   actor = interpret(toggleMachine).start();
-  state$ = from(this.actor);
 }
