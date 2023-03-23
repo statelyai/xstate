fd8747148c19608581e4ae0865bee3ad0a8e29cc
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-03-23 16:29:51 +0100

    Fixed Vue test

--- packages/xstate-vue/test/useInterpret.test.ts
+++ packages/xstate-vue/test/useInterpret.test.ts
@@ -2,19 +2,13 @@ import { render, fireEvent, waitFor } from '@testing-library/vue';
 import UseInterpret from './UseInterpret.vue';
 
 describe('useInterpret composable function', () => {
-  it('observer should be called with initial state', async () => {
-    const { getByTestId } = render(UseInterpret);
-
-    const buttonEl = getByTestId('button');
-    await waitFor(() => expect(buttonEl.textContent).toBe('Turn on'));
-  });
-
   it('observer should be called with next state', async () => {
     const { getByTestId } = render(UseInterpret);
 
     const buttonEl = getByTestId('button');
 
     await waitFor(() => expect(buttonEl.textContent).toBe('Turn on'));
+    debugger;
     await fireEvent.click(buttonEl);
     await waitFor(() => expect(buttonEl.textContent).toBe('Turn off'));
   });
