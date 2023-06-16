d4d6a6e18e4a5531637589d233021db9e316fad8
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-06-16 11:02:37 +0200

    move test from `useActorRef2.test.ts`

--- packages/xstate-vue/test/useActorRef.test.ts
+++ packages/xstate-vue/test/useActorRef.test.ts
@@ -1,7 +1,19 @@
 import { render, fireEvent, waitFor } from '@testing-library/vue';
 import UseActorRef from './UseActorRef.vue';
+import UseActorRef2 from './UseActorRef2.vue';
 
 describe('useActorRef composable function', () => {
+  it('should be able to create an actor ref from actor logic', async () => {
+    const { getByTestId } = render(UseActorRef2);
+    const button = getByTestId('count');
+
+    expect(button.textContent).toEqual('0');
+
+    await fireEvent.click(button);
+
+    expect(button.textContent).toEqual('1');
+  });
+
   it('observer should be called with next state', async () => {
     const { getByTestId } = render(UseActorRef);
 
