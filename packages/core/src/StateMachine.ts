9f5eb41b58ba5a0993200a36260257348f2abe95
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-06-20 13:23:47 +0200

    fixed type error

--- packages/core/src/StateMachine.ts
+++ packages/core/src/StateMachine.ts
@@ -293,7 +293,7 @@ export class StateMachine<
     preInitial._initial = true;
 
     if (actorCtx) {
-      const [nextState] = resolveActionsAndContext(
+      const nextState = resolveActionsAndContext(
         actions,
         initEvent as TEvent,
         preInitial,
