56e557aadcd05dd0ccf9ff08f5a77b1bdf12cab2
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-06-21 13:52:47 +0200

    Remove shebangs from workflow examples (#4089)

--- examples/workflow-event-based-service/main.ts
+++ examples/workflow-event-based-service/main.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env vite-node --script
 import { assign, createMachine, fromPromise, interpret } from 'xstate';
 
 async function delay(ms: number): Promise<void> {
--- examples/workflow-greeting/main.ts
+++ examples/workflow-greeting/main.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env vite-node --script
 import { assign, createMachine, fromPromise, interpret, waitFor } from 'xstate';
 
 // https://github.com/serverlessworkflow/specification/tree/main/examples#greeting-example
