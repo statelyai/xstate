ba165735d38159e7ce5302329e2e0ce0a34418b2
David Khourshid <davidkpiano@gmail.com>
2023-06-22 08:11:53 -0400

    Fix

--- packages/core/test/json.test.ts
+++ packages/core/test/json.test.ts
@@ -1,7 +1,7 @@
 import { createMachine, assign } from '../src/index';
 import * as machineSchema from '../src/machine.schema.json';
 
-import * as Ajv from 'ajv';
+const Ajv = require('ajv');
 
 const ajv = new Ajv();
 const validate = ajv.compile(machineSchema);
