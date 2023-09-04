b36b2275375ced5bd41363c9c18c9b288bcf09d6
David Khourshid <davidkpiano@gmail.com>
2023-09-04 18:35:59 -0400

    Tour WIP

---
+++ examples/tour-react/src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { getMachineShortestPaths } from '@xstate/graph';
+import { createMachine } from 'xstate';
+
+const tourMachine = createMachine({
+  id: 'tour',
+  initial: 'first',
+  states: {
+    first: {},
+    second: {},
+    third: {
+      type: 'final'
+    }
+  }
+});
+
+// const shortestPaths = getMachineShortestPaths(tourMachine);
+
+// console.log(shortestPaths);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
