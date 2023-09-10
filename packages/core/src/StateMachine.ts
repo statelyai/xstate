238b773b99ed500f9c62810a195b188bfaba3085
David Khourshid <davidkpiano@gmail.com>
2023-09-09 22:19:07 -0400

    This one too

--- packages/core/src/StateMachine.ts
+++ packages/core/src/StateMachine.ts
@@ -114,7 +114,6 @@ export class StateMachine<
 
   public states: StateNode<TContext, TEvent>['states'];
   public events: Array<TEvent['type']>;
-  public name = 'machine';
 
   constructor(
     /**
