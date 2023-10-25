010804eeef2c22ded20bec66494337160c11572d
David Khourshid <davidkpiano@gmail.com>
2023-10-25 10:51:42 -0700

    Add warning for sending events to non-started actors

--- packages/core/src/interpreter.ts
+++ packages/core/src/interpreter.ts
@@ -491,10 +491,17 @@ export class Actor<TLogic extends AnyActorLogic>
    * @param event The event to send
    */
   public send(event: EventFromLogic<TLogic>) {
-    if (isDevelopment && typeof event === 'string') {
-      throw new Error(
-        `Only event objects may be sent to actors; use .send({ type: "${event}" }) instead`
-      );
+    if (isDevelopment) {
+      if (typeof event === 'string') {
+        throw new Error(
+          `Only event objects may be sent to actors; use .send({ type: "${event}" }) instead`
+        );
+      }
+      if (this.status === ActorStatus.NotStarted) {
+        console.warn(
+          `Event "${event.type}" was sent to uninitialized actor "${this.id} (${this.sessionId})". This actor has not yet been started. Make sure to call ".start()" on the actor before sending events.`
+        );
+      }
     }
     this.system._relay(undefined, this, event);
   }
