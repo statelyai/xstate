0a6d4b2199ffbc96442a338f27abcefe7c2bd9c6
David Khourshid <davidkpiano@gmail.com>
2023-10-29 10:16:14 -0400

    Make inspection events consistent

--- packages/core/src/interpreter.ts
+++ packages/core/src/interpreter.ts
@@ -313,7 +313,7 @@ export class Actor<TLogic extends AnyActorLogic>
     this.system._sendInspectionEvent({
       type: '@xstate.event',
       sourceRef: this._parent,
-      targetRef: this,
+      actorRef: this,
       event: initEvent
     });
 
