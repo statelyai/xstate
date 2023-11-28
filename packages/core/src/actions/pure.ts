53299adc13a4b333d5b57d4bf33e37259a8107c0
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-11-28 11:53:14 +0100

    Reimplement `pure` using `enqueueActions`

--- packages/core/src/actions/pure.ts
+++ packages/core/src/actions/pure.ts
@@ -1,59 +1,13 @@
-import isDevelopment from '#is-development';
 import {
-  ActionArgs,
   Actions,
-  AnyActorScope,
-  AnyMachineSnapshot,
   EventObject,
   MachineContext,
   NoInfer,
   ParameterizedObject,
-  ProvidedActor,
-  SingleOrArray,
-  UnknownAction
+  ProvidedActor
 } from '../types.ts';
 import { toArray } from '../utils.ts';
-
-function resolvePure(
-  _: AnyActorScope,
-  state: AnyMachineSnapshot,
-  args: ActionArgs<any, any, any>,
-  _actionParams: ParameterizedObject['params'] | undefined,
-  {
-    get
-  }: {
-    get: ({
-      context,
-      event
-    }: {
-      context: MachineContext;
-      event: EventObject;
-    }) => SingleOrArray<UnknownAction> | undefined;
-  }
-) {
-  return [
-    state,
-    undefined,
-    toArray(get({ context: args.context, event: args.event }))
-  ];
-}
-
-export interface PureAction<
-  TContext extends MachineContext,
-  TExpressionEvent extends EventObject,
-  TEvent extends EventObject,
-  TActor extends ProvidedActor,
-  TAction extends ParameterizedObject,
-  TGuard extends ParameterizedObject,
-  TDelay extends string
-> {
-  (args: ActionArgs<TContext, TExpressionEvent, TEvent>, params: unknown): void;
-  _out_TEvent?: TEvent;
-  _out_TActor?: TActor;
-  _out_TAction?: TAction;
-  _out_TGuard?: TGuard;
-  _out_TDelay?: TDelay;
-}
+import { EnqueueActionsAction, enqueueActions } from './enqueueActions.ts';
 
 /**
  *
@@ -86,7 +40,7 @@ export function pure<
         TDelay
       >
     | undefined
-): PureAction<
+): EnqueueActionsAction<
   TContext,
   TExpressionEvent,
   TEvent,
@@ -95,19 +49,7 @@ export function pure<
   TGuard,
   TDelay
 > {
-  function pure(
-    args: ActionArgs<TContext, TExpressionEvent, TEvent>,
-    params: unknown
-  ) {
-    if (isDevelopment) {
-      throw new Error(`This isn't supposed to be called`);
-    }
-  }
-
-  pure.type = 'xstate.pure';
-  pure.get = getActions;
-
-  pure.resolve = resolvePure;
-
-  return pure;
+  return enqueueActions(({ context, event, enqueue }) => {
+    toArray(getActions({ context, event })).forEach(enqueue);
+  });
 }
