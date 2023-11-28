1639970c8aca9be9781661b03cedaf4ad6f5f5d9
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-11-28 13:19:45 +0100

    tweak things

--- packages/xstate-svelte/src/useActorRef.ts
+++ packages/xstate-svelte/src/useActorRef.ts
@@ -3,7 +3,7 @@ import { ActorOptions, ActorRefFrom, AnyActorLogic, createActor } from 'xstate';
 
 export function useActorRef<TLogic extends AnyActorLogic>(
   logic: TLogic,
-  options: ActorOptions<TLogic>
+  options?: ActorOptions<TLogic>
 ): ActorRefFrom<TLogic> {
   const actorRef = createActor(logic, options).start();
   onDestroy(() => actorRef.stop());
