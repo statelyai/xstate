80818017b23e4b6c8e40d0e64854f69ffc9f7307
David Khourshid <davidkpiano@gmail.com>
2023-12-06 09:18:55 -0500

    Fix higher-level logic for machines (#4513)
    * Add test
    
    * Add failing test
    
    * Bind getPersistedSnapshot
    
    * Changeset
    
    * Update packages/core/test/actorLogic.test.ts
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>
    
    * Update test
    
    ---------
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/core/test/actorLogic.test.ts
+++ packages/core/test/actorLogic.test.ts
@@ -5,7 +5,8 @@ import {
   createMachine,
   createActor,
   AnyActorLogic,
-  Snapshot
+  Snapshot,
+  ActorLogic
 } from '../src/index.ts';
 import {
   fromCallback,
@@ -981,5 +982,53 @@ describe('composable actor logic', () => {
         done();
       }
     });
+  });
+
+  it('higher-level logic wrapping a machine should be able to persist a snapshot', () => {
+    const logged: any[] = [];
+    function withLogging<T extends ActorLogic<any, any>>(actorLogic: T) {
+      const enhancedLogic: T = {
+        ...actorLogic,
+        transition: (state, event, actorCtx) => {
+          logged.push(event.type);
+          return actorLogic.transition(state, event, actorCtx);
+        }
+      };
+
+      return enhancedLogic;
+    }
+
+    const machine = createMachine({
+      initial: 'start',
+      states: {
+        start: {
+          on: { next: 'working' }
+        },
+        working: {
+          on: { more: 'done' }
+        },
+        done: {}
+      }
+    });
+
+    const actor = createActor(withLogging(machine)).start();
+
+    actor.send({ type: 'next' });
+    actor.send({ type: 'more' });
+
+    expect(logged).toEqual(['next', 'more']);
+
+    expect(actor.getSnapshot().value).toBe('done');
+
+    expect(() => {
+      actor.getPersistedSnapshot();
+    }).not.toThrow();
+
+    expect(actor.getPersistedSnapshot()).toEqual(
+      expect.objectContaining({
+        status: 'active',
+        value: 'done'
+      })
+    );
   });
 });
