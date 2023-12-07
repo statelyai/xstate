8c840b0f764e2e7afb5f6d95356670fc87c1b1bd
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-12-07 17:52:09 +0100

    remove redundant code

--- packages/core/src/toPromise.ts
+++ packages/core/src/toPromise.ts
@@ -25,12 +25,6 @@ import { AnyActorRef, OutputFrom } from './types.ts';
 export function toPromise<T extends AnyActorRef>(
   actor: T
 ): Promise<OutputFrom<T>> {
-  const currentSnapshot = actor.getSnapshot();
-
-  if (currentSnapshot.status === 'error') {
-    return Promise.reject(currentSnapshot.error);
-  }
-
   return new Promise((resolve, reject) => {
     actor.subscribe({
       complete: () => {
