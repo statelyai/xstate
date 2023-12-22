496b9bbf0d9c0ed76ac434c2b13df993c4deaffb
Kevin Maes <kevin@kevinmaes.com>
2023-12-22 12:26:54 +0100

    Auto commit from Stately Studio (#4614)

--- examples/friends-list-react/src/friend.machine.ts
+++ examples/friends-list-react/src/friend.machine.ts
@@ -1,6 +1,7 @@
 import { createMachine, assign } from 'xstate';
 
 export const friendMachine = createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QDMBOBLMA7CA6VYAhhOllAMQCiAIgJIAqA2gAwC6ioADgPazoAu6blg4gAHogAsAdmm4ArAEYAHNMkA2SQE4ATJOUBmLeoA0IAJ5SVuZVrtb509c3WKt0gwF9PZtJhy4kAKkFADKlPQA+gByAIIAspQs7EggPHyCwqISCAbqWrgqag75zLLMkvJmlgjqqgr2WpLMzO4GBjrevhjYeEGCZOShsQBqSWyi6cFZqTlO8rgG2nbq6nkuOsrViNKKcrb2OtIOm8ryXSB+vbiwhABuIeRisPyE-GC4hMjvqAAUii1mABKchXAK3B5kZKTXjTESzRCuSS4OrFHS6VbMKoWRAGZTKBr2fLSeQuSReHyXHo4cgAYVi0VplAAMtDUlNMvDQDk8QsjujFM55A47PJJNsELZCY1mkZ5AZzhcsNwIHBRGCIDCMkIueJcTpFIVVNp5KVytiaopDY0bTaKd1-HgCMQQlq4dkpCSbFpBeo9DoKqbTRK9MxCo18ooxcK6hcNYESAMoG7OR6EJU5EdlvIdHVc4GJfKCm57HLmIpNuo49S8BDXezYamEQg1MjhTL8UYFRLDASS3ZWsoZO5Fd4gA */
   id: 'friend',
   types: {} as {
     context: {
