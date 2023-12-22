fa1bd6cd212d6ab06b9d631992c1912ab88250f1
Kevin Maes <kevin@kevinmaes.com>
2023-12-22 15:35:27 +0100

    Auto commit from Stately Studio

--- examples/timer/src/timerMachine.ts
+++ examples/timer/src/timerMachine.ts
@@ -1,6 +1,7 @@
 import { assign, createMachine, fromCallback } from 'xstate';
 
 export const timerMachine = createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBbMAnAsgQwGMALVAOzAAIAKdQk8gSgDpZkB7ABw8gGJW8syANoAGALqJQHNrFRo2pSSAAeiACwiAbEwCMI-QCZNBgOwBOTQA4zlkwBoQAT0SWRBpgf0iAzJctqTAFY1TTMAXzCHNExcOjJKGjjGFnYuXnQyAFdkMFEJJBBpWXlFAtUEAwMdD0CvcwM-Mx0TbwdnBADtPUNak2bAnR1vCKiMbHxieOpaSeTWTm4IPjACBQg8pSK5VAUlcsrqg1r9esbm1qdEbyOmY5Fmy0DjG00RkGjxpISZ+jBmLEypFIZCgfFSGwKWxKe3UIhMui8Rm8mk0gUsOksbUQfXcnn03kC3j6lisajeH1is2+X3+gOBpFBABUAJIAYQA0hCpDJtrsyog9E8mKj9BYDGpvN4tPZLghNCY1EwvD4jpodAdLOSxpTftMaUwAUCQTw8AAbADueEcsC5hR50P5HR8wrMRJMmjhWi0OixcqMTBMXmuDRMnmRWpiE11iSpDB4WDgYGE4k29p2pVA5XVvsCBgikRApDYEDgSgpUamMd+DFTxXTMIQAFo1JjZY2dLmmGYvHpNBp-B7XgXy189bGUgtILXeRmVIgPb6Cd4mJZJQSGk9-JUI58qWPqwa6SDpw7M9i9B4jiJc-1XCZW+1UZYPEHpWZA+L82EgA */
   types: {} as {
     events:
       | { type: 'start' }
