2877f7a63f455802b0e2caeec67ca97a98212502
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-12-23 19:46:06 +0100

    Remove redundant test cases

--- packages/xstate-react/test/typegenTypes.test.tsx
+++ packages/xstate-react/test/typegenTypes.test.tsx
@@ -2,30 +2,6 @@ import { render } from '@testing-library/react';
 import { ActorRefFrom, assign, createMachine, TypegenMeta } from 'xstate';
 import { createActorContext, useActorRef, useMachine } from '../src/index.ts';
 
-describe('useMachine', () => {
-  it('should allow to be used with a machine without any missing implementations', () => {
-    interface TypesMeta extends TypegenMeta {
-      missingImplementations: {
-        actions: never;
-        delays: never;
-        guards: never;
-        actors: never;
-      };
-    }
-
-    const machine = createMachine({
-      types: { typegen: {} as TypesMeta }
-    });
-
-    function App() {
-      useMachine(machine);
-      return null;
-    }
-
-    render(<App />);
-  });
-});
-
 describe('useActorRef', () => {
   it('should handle multiple state.matches when passed TypegenMeta', () => {
     interface TypesMeta extends TypegenMeta {
@@ -70,146 +46,9 @@ describe('useActorRef', () => {
       }
     };
   });
-
-  it('returned actor created based on a machine that supplies missing implementations using `provide` should be assignable to the ActorRefFrom<...> type', () => {
-    interface TypesMeta extends TypegenMeta {
-      missingImplementations: {
-        actions: 'someAction';
-        delays: never;
-        guards: never;
-        actors: never;
-      };
-    }
-
-    const machine = createMachine({
-      types: { typegen: {} as TypesMeta }
-    });
-
-    function ChildComponent({}: { actorRef: ActorRefFrom<typeof machine> }) {
-      return null;
-    }
-
-    function App() {
-      const actorRef = useActorRef(
-        machine.provide({
-          actions: {
-            someAction: () => {}
-          }
-        })
-      );
-
-      return <ChildComponent actorRef={actorRef} />;
-    }
-
-    render(<App />);
-  });
-
-  it('returned actor created based on a machine that supplies missing implementations using `provide` should be assignable to the ActorRefFrom<...> type', () => {
-    interface TypesMeta extends TypegenMeta {
-      missingImplementations: {
-        actions: 'someAction';
-        delays: never;
-        guards: never;
-        actors: never;
-      };
-    }
-
-    const machine = createMachine({
-      types: { typegen: {} as TypesMeta }
-    });
-
-    function ChildComponent({}: { actorRef: ActorRefFrom<typeof machine> }) {
-      return null;
-    }
-
-    function App() {
-      const actorRef = useActorRef(
-        machine.provide({
-          actions: {
-            someAction: () => {}
-          }
-        })
-      );
-
-      return <ChildComponent actorRef={actorRef} />;
-    }
-
-    render(<App />);
-  });
 });
 
 describe('createActorContext', () => {
-  it('should allow to be used with a machine without any missing implementations', () => {
-    interface TypesMeta extends TypegenMeta {
-      missingImplementations: {
-        actions: never;
-        actors: never;
-        delays: never;
-        guards: never;
-      };
-    }
-
-    const machine = createMachine({
-      types: { typegen: {} as TypesMeta }
-    });
-
-    const Context = createActorContext(machine);
-
-    function App() {
-      return <Context.Provider>{null}</Context.Provider>;
-    }
-
-    render(<App />);
-  });
-
-  it('should allow to override already provided implementation', () => {
-    interface TypesMeta extends TypegenMeta {
-      missingImplementations: {
-        actions: 'fooAction';
-        actors: never;
-        delays: never;
-        guards: never;
-      };
-      eventsCausingActions: { fooAction: 'FOO' };
-      eventsCausingDelays: { barDelay: 'BAR' };
-    }
-
-    const machine = createMachine(
-      {
-        types: {
-          typegen: {} as TypesMeta,
-          events: {} as { type: 'FOO' } | { type: 'BAR' } | { type: 'BAZ' }
-        }
-      },
-      {
-        delays: {
-          barDelay: () => 42
-        }
-      }
-    );
-
-    const Context = createActorContext(machine);
-
-    function App() {
-      return (
-        <Context.Provider
-          logic={machine.provide({
-            actions: {
-              fooAction: () => {}
-            },
-            delays: {
-              barDelay: () => 100
-            }
-          })}
-        >
-          {null}
-        </Context.Provider>
-      );
-    }
-
-    render(<App />);
-  });
-
   it('should provide subset of the event type to action objects given in the `options` argument', () => {
     interface TypesMeta extends TypegenMeta {
       missingImplementations: {
@@ -246,50 +85,6 @@ describe('createActorContext', () => {
           })}
         >
           {null}
-        </Context.Provider>
-      );
-    }
-
-    render(<App />);
-  });
-
-  it('returned actor created based on a machine that supplies missing implementations using `provide` should be assignable to the ActorRefFrom<...> type', () => {
-    interface TypesMeta extends TypegenMeta {
-      missingImplementations: {
-        actions: 'someAction';
-        actors: never;
-        delays: never;
-        guards: never;
-      };
-    }
-
-    const machine = createMachine({
-      types: { typegen: {} as TypesMeta }
-    });
-
-    const Context = createActorContext(
-      machine.provide({
-        actions: {
-          someAction: () => {}
-        }
-      })
-    );
-
-    function GrandchildComponent({}: {
-      actorRef: ActorRefFrom<typeof machine>;
-    }) {
-      return null;
-    }
-
-    function ChildComponent() {
-      const actorRef = Context.useActorRef();
-      return <GrandchildComponent actorRef={actorRef} />;
-    }
-
-    function App() {
-      return (
-        <Context.Provider>
-          <ChildComponent />
         </Context.Provider>
       );
     }
