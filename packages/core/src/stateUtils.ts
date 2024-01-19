4f4fc3d650d97109fe11c567a6727a4d06eee032
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-01-19 15:19:51 +0100

    remove outdated comment

--- packages/core/src/stateUtils.ts
+++ packages/core/src/stateUtils.ts
@@ -233,9 +233,6 @@ export function getCandidates<TEvent extends EventObject>(
           );
         }
 
-        // split the event descriptor from unescaped periods
-        // e.g. 'foo.bar' is ['foo','bar']
-        // e.g. 'foo\.bar' is ['foo\.bar']
         const partialEventTokens = eventDescriptor.split('.');
         const eventTokens = receivedEventType.split('.');
 
