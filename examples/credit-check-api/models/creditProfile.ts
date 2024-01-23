1a530890e21a2d064ebeade30b35e8cd9fb22fa4
Gavin Bauman <gavinbauman1322@gmail.com>
2024-01-23 10:09:29 -0500

    update models with JS shorthand

--- examples/credit-check-api/models/creditProfile.ts
+++ examples/credit-check-api/models/creditProfile.ts
@@ -1,35 +1,16 @@
 import { ObjectId } from "mongodb";
 
 export default class CreditProfile {
-  _id?: ObjectId;
-  SSN: string;
-  LastName: string;
-  FirstName: string;
-  GavUnionScore: number;
-  EquiGavinScore: number;
-  GavperianScore: number;
-  ErrorMessage: string;
-  MiddleScore: number;
-  InterestRateOptions: number[];
   constructor(
-    SSN: string,
-    LastName: string,
-    FirstName: string,
-    GavUnionScore: number,
-    EquiGavinScore: number,
-    GavperianScore: number,
-    ErrorMessage: string,
-    MiddleScore: number,
-    InterestRateOptions: number[]
-  ) {
-    this.SSN = SSN;
-    this.LastName = LastName;
-    this.FirstName = FirstName;
-    this.GavUnionScore = GavUnionScore;
-    this.EquiGavinScore = EquiGavinScore;
-    this.GavperianScore = GavperianScore;
-    this.ErrorMessage = ErrorMessage;
-    this.MiddleScore = MiddleScore;
-    this.InterestRateOptions = InterestRateOptions;
-  }
+    public SSN: string,
+    public LastName: string,
+    public FirstName: string,
+    public GavUnionScore: number,
+    public EquiGavinScore: number,
+    public GavperianScore: number,
+    public ErrorMessage: string,
+    public MiddleScore: number,
+    public InterestRateOptions: number[],
+    public _id?: ObjectId
+  ) {}
 }
--- examples/credit-check-api/models/creditReport.ts
+++ examples/credit-check-api/models/creditReport.ts
@@ -1,13 +1,10 @@
 import { ObjectId } from "mongodb";
 
 export default class CreditReport {
-  _id?: ObjectId;
-  ssn: string;
-  bureauName: string;
-  creditScore: number;
-  constructor(ssn: string, bureauName: string, creditScore: number) {
-    this.ssn = ssn;
-    this.bureauName = bureauName;
-    this.creditScore = creditScore;
-  }
+  constructor(
+    public ssn: string,
+    public bureauName: string,
+    public creditScore: number,
+    public _id?: ObjectId
+  ) {}
 }
