60eabd08432464f86d04942cf6224ad217225fc4
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2024-01-29 10:05:55 +0000

    Appease TypeScriptGod

--- examples/7guis-2-temperature-vue/src/TempConverter.vue
+++ examples/7guis-2-temperature-vue/src/TempConverter.vue
@@ -8,10 +8,10 @@ const { snapshot, send } = useMachine(tempMachine);
 <template>
   <div>
     <label for="fahrenheit">Fahrenheit</label>
-    <input @input="(event) => send({ type: 'changeF', value: event.target.value })" type="text"
+    <input @input="(event) => send({ type: 'changeF', value: (event!.target as HTMLInputElement)!.value })" type="text"
       :value="snapshot.context.fahrenheit" id="fahrenheit" size="10" />
     <label for="celsius">Celsius</label>
-    <input @input="(event) => send({ type: 'changeC', value: event.target.value })" :value="snapshot.context.celsius"
-      type="text" id="celsius" size="10" />
+    <input @input="(event) => send({ type: 'changeC', value: (event!.target as HTMLInputElement)!.value })"
+      :value="snapshot.context.celsius" type="text" id="celsius" size="10" />
   </div>
 </template>
