43c2d33eca61971b6a389dbf954472a9065761de
David Khourshid <davidkpiano@gmail.com>
2024-02-18 15:02:34 -0500

    Cleanup + add select()

--- packages/xstate-store/src/index.ts
+++ packages/xstate-store/src/index.ts
@@ -1,11 +1,11 @@
-import {
-  Compute,
+import type {
+  EventObject,
   InteropSubscribable,
+  MachineContext,
   Values,
-  toObserver,
-  type InteropObservable,
-  type Observer,
-  type Subscribable
+  InteropObservable,
+  Observer,
+  Subscribable
 } from 'xstate';
 import { symbolObservable } from '../../core/src/symbolObservable';
 
@@ -13,34 +13,62 @@ function deepClone(val: any) {
   return JSON.parse(JSON.stringify(val));
 }
 
-interface Store<T, Ev extends EventPayloadMap>
+export function toObserver<T>(
+  nextHandler?: Observer<T> | ((value: T) => void),
+  errorHandler?: (error: any) => void,
+  completionHandler?: () => void
+): Observer<T> {
+  const isObserver = typeof nextHandler === 'object';
+  const self = isObserver ? nextHandler : undefined;
+
+  return {
+    next: (isObserver ? nextHandler.next : nextHandler)?.bind(self),
+    error: (isObserver ? nextHandler.error : errorHandler)?.bind(self),
+    complete: (isObserver ? nextHandler.complete : completionHandler)?.bind(
+      self
+    )
+  };
+}
+
+interface Store<T, Ev extends EventObject>
   extends Subscribable<T>,
     InteropObservable<T> {
-  send: (event: Compute<Foo<Ev>>) => void;
+  send: (event: Ev) => void;
   getSnapshot: () => T;
   getInitialSnapshot: () => T;
+  select: <TSelected>(selector: (ctx: T) => TSelected) => TSelected;
 }
 
 type EventPayloadMap = Record<string, {}>;
 
-type Foo<T extends EventPayloadMap> = Values<{
+type ExtractEventsFromPayloadMap<T extends EventPayloadMap> = Values<{
   [K in keyof T & string]: T[K] & { type: K };
 }>;
 
-export function createStore<T, TEventPayloadMap extends EventPayloadMap>(
-  context: T,
+export function createStore<
+  TContext extends MachineContext,
+  TEventPayloadMap extends EventPayloadMap
+>(
+  context: TContext,
   transitions?: {
-    [K in keyof TEventPayloadMap]: (ctx: T, ev: TEventPayloadMap[K]) => void;
+    [K in keyof TEventPayloadMap]: (
+      ctx: TContext,
+      ev: TEventPayloadMap[K]
+    ) => void;
   }
-): Store<T, TEventPayloadMap> & {
-  [K in keyof TEventPayloadMap]: (
-    payload: Compute<Omit<TEventPayloadMap[K], 'type'>>
-  ) => void;
-} {
+): Store<TContext, ExtractEventsFromPayloadMap<TEventPayloadMap>> {
+  // & {
+  //   [K in keyof TEventPayloadMap]: (
+  //     payload: Compute<Omit<TEventPayloadMap[K], 'type'>>
+  //   ) => void;
+  // }
   const initialCtx = deepClone(context);
   let ctx = deepClone(context);
-  function receive(ev: Foo<TEventPayloadMap>) {
-    const fn = transitions?.[ev.type as Foo<TEventPayloadMap>['type']];
+  function receive(ev: ExtractEventsFromPayloadMap<TEventPayloadMap>) {
+    const fn =
+      transitions?.[
+        ev.type as ExtractEventsFromPayloadMap<TEventPayloadMap>['type']
+      ];
 
     fn?.(ctx, ev as any);
     const cloned = deepClone(ctx);
@@ -48,10 +76,16 @@ export function createStore<T, TEventPayloadMap extends EventPayloadMap>(
   }
   let observers: Set<Observer<any>> | undefined;
 
-  const o: Store<T, Foo<TEventPayloadMap>> = {
-    // @ts-ignore
-    send(ev: Ev) {
-      receive(ev);
+  const store: Store<
+    TContext,
+    ExtractEventsFromPayloadMap<TEventPayloadMap>
+  > = {
+    send(ev) {
+      receive(ev as unknown as ExtractEventsFromPayloadMap<TEventPayloadMap>);
+    },
+    select<TSelected>(selector: (ctx: TContext) => TSelected) {
+      const selected = selector(ctx);
+      return deepClone(selected);
     },
     getSnapshot() {
       return deepClone(ctx);
@@ -75,11 +109,11 @@ export function createStore<T, TEventPayloadMap extends EventPayloadMap>(
     }
   };
 
-  if (transitions) {
-    for (const key of Object.keys(transitions)) {
-      o[key] = (ev) => receive({ ...ev, type: key });
-    }
-  }
+  // if (transitions) {
+  //   for (const key of Object.keys(transitions)) {
+  //     o[key] = (ev) => receive({ ...ev, type: key });
+  //   }
+  // }
 
-  return o as any;
+  return store;
 }
--- packages/xstate-store/test/store.test.ts
+++ packages/xstate-store/test/store.test.ts
@@ -8,35 +8,33 @@ it('creates a store API', () => {
       "@@observable": [Function],
       "getInitialSnapshot": [Function],
       "getSnapshot": [Function],
+      "select": [Function],
       "send": [Function],
       "subscribe": [Function],
     }
   `);
 });
 
-it('1', () => {
-  const store = createStore(
-    {
-      count: 0
-    },
-    {
-      inc: (c, ev: { type: 'inc'; by: number }) => {
-        c.count += ev.by;
-      }
+it('updates a store with an event without mutating original context', () => {
+  const context = { count: 0 };
+  const store = createStore(context, {
+    inc: (c, ev: { type: 'inc'; by: number }) => {
+      c.count += ev.by;
     }
-  );
+  });
 
   const initial = store.getInitialSnapshot();
 
-  store.inc({ by: 1 });
+  store.send({ type: 'inc', by: 1 });
 
   const next = store.getSnapshot();
 
   expect(initial).toEqual({ count: 0 });
   expect(next).toEqual({ count: 1 });
+  expect(context.count).toEqual(0);
 });
 
-it('store', () => {
+it('updates state from sent events', () => {
   const store = createStore(
     {
       count: 0
@@ -52,20 +50,31 @@ it('store', () => {
   );
 
   store.send({ type: 'inc', by: 9 });
-  store.inc({ by: 1 });
-  store.send({ type: 'inc', by: 3 });
-  store.dec({ by: 3 });
+  store.send({ type: 'dec', by: 3 });
 
-  expect(store.getSnapshot()).toEqual({ count: 10 });
+  expect(store.getSnapshot()).toEqual({ count: 6 });
 });
 
-it('works with any context', () => {
-  const store = createStore(0, {
-    inc: (ctx) => {
-      ctx++;
-    },
-    dec: (ctx) => {
-      ctx--;
-    }
+it('selects values from context', () => {
+  const store = createStore({
+    users: [
+      {
+        name: 'David',
+        pets: [
+          {
+            type: 'dog',
+            name: 'Maki'
+          },
+          {
+            type: 'dog',
+            name: 'Ato'
+          }
+        ]
+      }
+    ]
   });
+
+  const firstPet = store.select((s) => s.users[0]?.pets[0]?.name);
+
+  expect(firstPet).toBe('Maki');
 });
