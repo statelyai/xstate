40ebdaa8762fda601ca007c5998c4cb2753ea086
oscar marina <oscar7600@outlook.com>
2024-03-07 20:32:26 +0100

    [@xstate/lit] Remove Lit warn

--- scripts/jest-utils/setup.js
+++ scripts/jest-utils/setup.js
@@ -20,18 +20,20 @@ afterEach(() => {
     const spy = consoleSpies[method];
 
     if (spy.mock.calls.length) {
+      spy.mock.calls = spy.mock.calls.filter(
+        (args) =>
+          !args.includes(
+            'Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.'
+          )
+      );
+    }
+
+    if (spy.mock.calls.length) {
       const calls = spy.mock.calls;
 
       spy.mockRestore();
       // actually log the "unobserved" calls to the console to make them observable in the test output
-      calls
-        .filter(
-          (args) =>
-            !args.includes(
-              'Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.'
-            )
-        )
-        .forEach((args) => console[method](...args));
+      calls.forEach((args) => console[method](...args));
       // as we just restored the mock, we need to setup a new spy
       consoleSpies[method] = spyOnConsole(method);
 
