299d1da8fa9f3b1f5c0f7454b470bec173fdfff3
Cristian Velasquez Ramos <seeve.c@gmail.com>
2024-04-23 09:07:24 -0400

    revert unintended changes

--- examples/7guis-flight-booker-react/src/App.tsx
+++ examples/7guis-flight-booker-react/src/App.tsx
@@ -1,7 +1,7 @@
-import { FlightContext } from './machine';
-import { DateInput } from './DateInput';
-import { useRef, useState } from 'react';
-import { X } from 'lucide-react';
+import { FlightContext } from "./machine";
+import { DateInput } from "./DateInput";
+import { useRef, useState } from "react";
+import { X } from "lucide-react";
 
 const Flight = () => {
   const dialogRef = useRef<HTMLDialogElement | null>(null);
@@ -10,9 +10,9 @@ const Flight = () => {
 
   const { startDate, returnDate } = state.context;
   const isValidDate = startDate && (!returnDate || returnDate >= startDate);
-  const canSubmit = state.matches('booking') && isValidDate;
+  const canSubmit = state.matches("booking") && isValidDate;
 
-  const trip = state.matches({ booking: 'roundTrip' }) ? 'roundTrip' : 'oneWay';
+  const trip = state.matches({ booking: "roundTrip" }) ? "roundTrip" : "oneWay";
 
   return (
     <section>
@@ -20,7 +20,7 @@ const Flight = () => {
         Book a flight
       </button>
       <dialog ref={dialogRef}>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
+        <div style={{ display: "flex", alignItems: "center", gap: "12px" }}>
           <h1>
             Book Flight
             <button
@@ -33,10 +33,10 @@ const Flight = () => {
             </button>
           </h1>
         </div>
-        <form style={{ display: 'flex', flexDirection: 'column' }}>
+        <form style={{ display: "flex", flexDirection: "column" }}>
           <select
             onChange={() => {
-              send({ type: 'CHANGE_TRIP' });
+              send({ type: "CHANGE_TRIP" });
             }}
             value={trip}
           >
@@ -46,25 +46,25 @@ const Flight = () => {
           <DateInput
             value={startDate}
             onChange={(value: string) =>
-              send({ type: 'CHANGE_START_DATE', value })
+              send({ type: "CHANGE_START_DATE", value })
             }
             label="Start date"
           />
           <DateInput
             value={returnDate}
             onChange={(value: string) =>
-              send({ type: 'CHANGE_RETURN_DATE', value })
+              send({ type: "CHANGE_RETURN_DATE", value })
             }
-            disabled={trip === 'oneWay' || !startDate}
+            disabled={trip === "oneWay" || !startDate}
             label="Return date"
           />
           <button
             type="button"
-            onClick={() => send({ type: 'BOOK' })}
+            onClick={() => send({ type: "BOOK" })}
             disabled={!canSubmit}
           >
-            {state.matches('booking') && 'Book'}
-            {state.matches('booked') && 'Success!'}
+            {state.matches("booking") && "Book"}
+            {state.matches("booked") && "Success!"}
           </button>
         </form>
       </dialog>
--- examples/7guis-flight-booker-react/src/machine.ts
+++ examples/7guis-flight-booker-react/src/machine.ts
@@ -1,7 +1,7 @@
-import { setup, assign, assertEvent } from 'xstate';
-import { createActorContext } from '@xstate/react';
+import { setup, assign, assertEvent } from "xstate";
+import { createActorContext } from "@xstate/react";
 
-const TODAY = new Date().toISOString().split('T')[0];
+const TODAY = new Date().toISOString().split("T")[0];
 
 type FlightData = {
   startDate?: string;
@@ -11,64 +11,64 @@ export const flightBookerMachine = setup({
   types: {
     context: {} as FlightData,
     events: {} as
-      | { type: 'BOOK' }
-      | { type: 'CHANGE_TRIP' }
-      | { type: 'CHANGE_START_DATE'; value: string }
-      | { type: 'CHANGE_RETURN_DATE'; value: string }
+      | { type: "BOOK" }
+      | { type: "CHANGE_TRIP" }
+      | { type: "CHANGE_START_DATE"; value: string }
+      | { type: "CHANGE_RETURN_DATE"; value: string },
   },
   actions: {
     setStartDate: assign(({ event }) => {
-      assertEvent(event, 'CHANGE_START_DATE');
+      assertEvent(event, "CHANGE_START_DATE");
       return { startDate: event.value };
     }),
     setReturnDate: assign(({ event }) => {
-      assertEvent(event, 'CHANGE_RETURN_DATE');
+      assertEvent(event, "CHANGE_RETURN_DATE");
       return { returnDate: event.value };
-    })
+    }),
   },
-  guards: {}
+  guards: {},
 }).createMachine({
   context: {
     startDate: TODAY,
-    returnDate: TODAY
+    returnDate: TODAY,
   },
-  id: 'flightBookerMachine',
-  initial: 'booking',
+  id: "flightBookerMachine",
+  initial: "booking",
   states: {
     booking: {
-      initial: 'oneWay',
+      initial: "oneWay",
       on: {
         CHANGE_START_DATE: {
-          actions: 'setStartDate'
+          actions: "setStartDate",
         },
         BOOK: {
-          target: 'booked'
-        }
+          target: "booked",
+        },
       },
       states: {
         oneWay: {
           on: {
             CHANGE_TRIP: {
-              target: 'roundTrip'
-            }
-          }
+              target: "roundTrip",
+            },
+          },
         },
         roundTrip: {
           on: {
             CHANGE_TRIP: {
-              target: 'oneWay'
+              target: "oneWay",
             },
             CHANGE_RETURN_DATE: {
-              actions: 'setReturnDate'
-            }
-          }
-        }
-      }
+              actions: "setReturnDate",
+            },
+          },
+        },
+      },
     },
     booked: {
-      type: 'final'
-    }
-  }
+      type: "final",
+    },
+  },
 });
 
 export const FlightContext = createActorContext(flightBookerMachine);
