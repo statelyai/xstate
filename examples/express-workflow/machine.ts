4db301f624a845818911d7c6894d8a5562a35dfd
David Khourshid <davidkpiano@gmail.com>
2024-05-02 10:14:10 -0400

    Auto commit from Stately Studio

--- examples/express-workflow/machine.ts
+++ examples/express-workflow/machine.ts
@@ -1,6 +1,7 @@
 import { createMachine, assign } from 'xstate';
 
 export const machine = createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QGMD2BXAdgFzAJwDoo8wxMBiAFQEkBZAUQCUBtABgF1FQAHVWAS2z9UmLiAAeiAMysAbABoQAT0QAmAJyyAvlsVosuQkrAAbE6gDuVOkzackIXgKEixkhDIXLEAVgCM2rog+jj4BCQQ1gwsHGJOgsKiDu6yrIoqCKo+ABw6ehihRqbmVvQAbmTYAASqdnF8Ca7JiAAsrOrp0rKqBFKyAOx+2f0+ecEFhgQAcmAWVbDYAIa4VX7k5ZWrdQ7xLkmg7kPqBH5t3T6dCAPHAax+Puc6QZioEHBiIYb1zoluiAC0Uj8vVY2R86ikI0uflUY0+YWIpH2jgaez+CD8Uik0NkgXyBjCxjMlm+jWR7hk-UuLVULThEzCEVJaOaGNkBFk6lOciyl3BwPUPiBWXpBMIMzmC2WYFWzN+rP82QIrH62RhF28CH8oyeQA */
   id: 'counter',
   initial: 'green',
   context: {
