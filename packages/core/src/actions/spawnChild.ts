8b936ab18dcb2bbf5881b9187d8519e2f9bb418b
David Khourshid <davidkpiano@gmail.com>
2024-07-03 11:15:41 -0400

    Update packages/core/src/actions/spawnChild.ts
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/core/src/actions/spawnChild.ts
+++ packages/core/src/actions/spawnChild.ts
@@ -1,7 +1,6 @@
 import isDevelopment from '#is-development';
 import { cloneMachineSnapshot } from '../State.ts';
 import { ProcessingStatus, createActor } from '../createActor.ts';
-// import { executingCustomAction } from '../createActor.ts';
 import {
   ActionArgs,
   ActionFunction,
