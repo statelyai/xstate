6de31d371739edbdfaab4fcbf59574942b5ac302
David Khourshid <davidkpiano@gmail.com>
2024-07-10 19:12:46 -0400

    Clean up State

--- packages/core/src/State.ts
+++ packages/core/src/State.ts
@@ -143,9 +143,6 @@ interface MachineSnapshotBase<
     TMeta | undefined // States might not have meta defined
   >;
 
-  stateId: StateId<TConfig>;
-  config: TConfig;
-
   toJSON: () => unknown;
 }
 
