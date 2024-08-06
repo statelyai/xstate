e16b4b8f080c7b4979c0d9a23d44bd88c09f5239
with-heart <with.heart+git@pm.me>
2024-08-06 11:32:33 -0400

    remove jest config/dependencies/scripts

--- scripts/jest-utils/index.js
+++
@@ -1,6 +0,0 @@
-const { consoleSpies } = require('./console-spies');
-
-module.exports.clearConsoleMocks = () =>
-  Object.values(consoleSpies).forEach((spy) => spy.mockClear());
-
-module.exports.sleep = (ms) => new Promise((res) => setTimeout(res, ms));
