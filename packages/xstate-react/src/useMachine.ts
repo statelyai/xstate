4e8a87036659ceb8a81a4f40439ea7f4cced0a9e
SandroMaglione <lass.maglio@gmail.com>
2024-08-25 12:09:54 +0200

    `useMachine` required input when defined

--- packages/xstate-react/src/useMachine.ts
+++ packages/xstate-react/src/useMachine.ts
@@ -1,10 +1,25 @@
-import { Actor, ActorOptions, AnyStateMachine, StateFrom } from 'xstate';
+import {
+  Actor,
+  ActorOptions,
+  AnyStateMachine,
+  StateFrom,
+  type ConditionalRequired,
+  type IsNotNever,
+  type RequiredOptions
+} from 'xstate';
 import { useActor } from './useActor.ts';
 
 /** @alias useActor */
 export function useMachine<TMachine extends AnyStateMachine>(
   machine: TMachine,
-  options: ActorOptions<TMachine> = {}
+  ...[options]: ConditionalRequired<
+    [
+      options?: ActorOptions<TMachine> & {
+        [K in RequiredOptions<TMachine>]: unknown;
+      }
+    ],
+    IsNotNever<RequiredOptions<TMachine>>
+  >
 ): [StateFrom<TMachine>, Actor<TMachine>['send'], Actor<TMachine>] {
   return useActor(machine, options);
 }
