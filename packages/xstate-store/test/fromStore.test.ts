87012f2da65cef79e57fde3a2c4d9f4a6fcf3b5f
David Khourshid <davidkpiano@gmail.com>
2024-09-10 08:56:33 -0400

    Update packages/xstate-store/test/fromStore.test.ts
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/xstate-store/test/fromStore.test.ts
+++ packages/xstate-store/test/fromStore.test.ts
@@ -21,27 +21,4 @@ describe('fromStore', () => {
 
     expect(actor.getSnapshot().context.count).toEqual(50);
   });
-
-  // it('creates an actor from store logic with input (1 arg)', () => {
-  //   const storeLogic = fromStore({
-  //     context: (count: number) => ({ count }),
-  //     on: {
-  //       inc: {
-  //         count: (ctx, ev: { by: number }) => {
-  //           return ctx.count + ev.by;
-  //         }
-  //       }
-  //     }
-  //   });
-
-  //   const actor = createActor(storeLogic, {
-  //     input: 42
-  //   });
-
-  //   actor.start();
-
-  //   actor.send({ type: 'inc', by: 8 });
-
-  //   expect(actor.getSnapshot().context.count).toEqual(50);
-  // });
 });
