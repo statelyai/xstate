bc9a8989a523382c886f175568fc682854133f93
David Khourshid <davidkpiano@gmail.com>
2024-09-16 23:32:50 -0400

    Add simple example

---
+++ packages/xstate-store/examples/counter-react/src/App.tsx
@@ -0,0 +1,55 @@
+import './App.css';
+import { createStore } from '@xstate/store';
+import { useSelector } from '@xstate/store/react';
+import { useEffect } from 'react';
+
+const store = createStore({
+  context: {
+    count: 0
+  },
+  on: {
+    inc: (context, event: { by: number }) => {
+      return {
+        count: context.count + event.by
+      };
+    },
+    reset: (_context, _ev, { emit }) => {
+      emit({ type: 'reset' });
+      return {
+        count: 0
+      };
+    }
+  }
+});
+
+function App() {
+  const count = useSelector(store, (s) => s.context.count);
+
+  useEffect(() => {
+    const sub = store.on('reset', () => {
+      console.log('Count reset!');
+    });
+
+    return sub.unsubscribe;
+  }, []);
+
+  return (
+    <>
+      <div
+        className="card"
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '.5rem'
+        }}
+      >
+        <button onClick={() => store.send({ type: 'inc', by: 1 })}>
+          count is {count}
+        </button>
+        <button onClick={() => store.send({ type: 'reset' })}>reset</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
