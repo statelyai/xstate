c917b1c10206d42ec61450d3067e18f22f459ba8
David Khourshid <davidkpiano@gmail.com>
2024-09-17 20:00:31 -0400

    Cleanup

--- packages/xstate-store/src/useSyncExternalStoreWithSelector.ts
+++ packages/xstate-store/src/useSyncExternalStoreWithSelector.ts
@@ -3,8 +3,6 @@ import * as React from 'react';
 const is = (a: any, b: any) => a === b;
 import { useSyncExternalStore } from 'react';
 
-// Intentionally not using named imports because Rollup uses dynamic dispatch
-// for CommonJS interop.
 const { useRef, useEffect, useMemo, useDebugValue } = React;
 
 // Same as useSyncExternalStore, but supports selector and isEqual arguments.
@@ -15,7 +13,6 @@ export function useSyncExternalStoreWithSelector<Snapshot, Selection>(
   selector: (snapshot: Snapshot) => Selection,
   isEqual?: (a: Selection, b: Selection) => boolean
 ): Selection {
-  // Use this to track the rendered snapshot.
   const instRef = useRef<
     | {
         hasValue: true;
@@ -39,23 +36,15 @@ export function useSyncExternalStoreWithSelector<Snapshot, Selection>(
   }
 
   const [getSelection, getServerSelection] = useMemo(() => {
-    // Track the memoized state using closure variables that are local to this
-    // memoized instance of a getSnapshot function. Intentionally not using a
-    // useRef hook, because that state would be shared across all concurrent
-    // copies of the hook/component.
     let hasMemo = false;
     let memoizedSnapshot: Snapshot;
     let memoizedSelection: Selection;
     const memoizedSelector = (nextSnapshot: Snapshot) => {
       if (!hasMemo) {
-        // The first time the hook is called, there is no memoized result.
         hasMemo = true;
         memoizedSnapshot = nextSnapshot;
         const nextSelection = selector(nextSnapshot);
         if (isEqual !== undefined) {
-          // Even if the selector has changed, the currently rendered selection
-          // may be equal to the new selection. We should attempt to reuse the
-          // current value if possible, to preserve downstream memoizations.
           if (inst.hasValue) {
             const currentSelection = inst.value;
             if (isEqual(currentSelection, nextSelection)) {
@@ -68,25 +57,16 @@ export function useSyncExternalStoreWithSelector<Snapshot, Selection>(
         return nextSelection;
       }
 
-      // We may be able to reuse the previous invocation's result.
       const prevSnapshot: Snapshot = memoizedSnapshot;
       const prevSelection: Selection = memoizedSelection;
 
       if (is(prevSnapshot, nextSnapshot)) {
-        // The snapshot is the same as last time. Reuse the previous selection.
         return prevSelection;
       }
 
-      // The snapshot has changed, so we need to compute a new selection.
       const nextSelection = selector(nextSnapshot);
 
-      // If a custom isEqual function is provided, use that to check if the data
-      // has changed. If it hasn't, return the previous selection. That signals
-      // to React that the selections are conceptually equal, and we can bail
-      // out of rendering.
       if (isEqual !== undefined && isEqual(prevSelection, nextSelection)) {
-        // The snapshot still has changed, so make sure to update to not keep
-        // old references alive
         memoizedSnapshot = nextSnapshot;
         return prevSelection;
       }
@@ -95,7 +75,6 @@ export function useSyncExternalStoreWithSelector<Snapshot, Selection>(
       memoizedSelection = nextSelection;
       return nextSelection;
     };
-    // Assigning this to a constant so that Flow knows it can't change.
     const maybeGetServerSnapshot =
       getServerSnapshot === undefined ? null : getServerSnapshot;
     const getSnapshotWithSelector = () => memoizedSelector(getSnapshot());
@@ -113,9 +92,7 @@ export function useSyncExternalStoreWithSelector<Snapshot, Selection>(
   );
 
   useEffect(() => {
-    // $FlowFixMe[incompatible-type] changing the variant using mutation isn't supported
     inst.hasValue = true;
-    // $FlowFixMe[incompatible-type]
     inst.value = value;
   }, [value]);
 
