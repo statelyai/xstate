eea56bcb548c26716896d6bc946efdfb1f42bbac
Farzad Yousefzadeh <farskid@gmail.com>
2025-10-07 00:27:43 +0300

    Add system.getRunningActors method

--- packages/core/src/system.ts
+++ packages/core/src/system.ts
@@ -55,6 +55,7 @@ export interface ActorSystem<T extends ActorSystemInfo> {
   /** @internal */
   _set: <K extends keyof T['actors']>(key: K, actorRef: T['actors'][K]) => void;
   get: <K extends keyof T['actors']>(key: K) => T['actors'][K] | undefined;
+  getRunningActors: () => T['actors'];
 
   inspect: (
     observer:
@@ -187,6 +188,9 @@ export function createSystem<T extends ActorSystemInfo>(
     },
     get: (systemId) => {
       return keyedActors.get(systemId) as T['actors'][any];
+    },
+    getRunningActors: () => {
+      return Object.fromEntries(keyedActors.entries()) as T['actors'];
     },
     _set: (systemId, actorRef) => {
       const existing = keyedActors.get(systemId);
