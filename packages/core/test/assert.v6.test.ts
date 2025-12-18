c81c75fff2057a31dfda91c7e51602ca9dbed908
David Khourshid <davidkpiano@gmail.com>
2025-12-17 22:48:24 -0500

    Fix assert tests

--- packages/core/test/assert.v6.test.ts
+++ packages/core/test/assert.v6.test.ts
@@ -42,7 +42,7 @@ describe('assertion helpers', () => {
     actor.subscribe({
       error(err) {
         expect(err).toMatchInlineSnapshot(
-          `[Error: Expected event {"type":"count","value":42} to have type "greet"]`
+          `[Error: Expected event {"type":"count","value":42} to have type matching "greet"]`
         );
         resolve();
       }
@@ -106,7 +106,7 @@ describe('assertion helpers', () => {
     actor.subscribe({
       error(err) {
         expect(err).toMatchInlineSnapshot(
-          `[Error: Expected event {"type":"count","value":42} to have one of types "greet", "notify"]`
+          `[Error: Expected event {"type":"count","value":42} to have one of types matching "greet", "notify"]`
         );
         resolve();
       }
