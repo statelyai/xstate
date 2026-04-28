9a9b0443c377b14323f4f325e67a1dc6611d2894
David Khourshid <davidkpiano@gmail.com>
2026-04-28 17:46:02 -0400

    Auto commit from Stately Studio

--- examples/video-player-react/src/toggle.machine.ts
+++ examples/video-player-react/src/toggle.machine.ts
@@ -5,6 +5,7 @@ export interface ToggleContext {
 }
 
 export const toggleMachine = createMachine<ToggleContext>({
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoCWAdgIYDGyeAbmAMQAqA8gOKMAyAogNoAMAuoqAAdUsPOVQF+IAB6IAjAE4AHABoQAT0QAmRQDYcOg4aOGAzAF8zqtBmw5S5KnSatOvSUJFiJSaXKWqNBAU9Y1DDCyt0LFx7Sho2KgJkAAJNbj4fD1E8cUkZBBN5EwDEEwAWMotLEAJUCDhJa2j3YWzcn3zZTRKgzQiQJttCWKoWzxzvUHydLh7NLnNqwZiyOLG2yd8Cstn1OR0AVn7lnAA5MAB3ZNhkImQwZNl1rzzSxVkencqqoA */
   id: 'toggle',
   initial: 'inactive',
   context: {
@@ -14,9 +15,15 @@ export const toggleMachine = createMachine<ToggleContext>({
     inactive: {
       on: { TOGGLE: 'active' }
     },
+
     active: {
-      entry: assign({ count: (ctx) => ctx.count + 1 }),
-      on: { TOGGLE: 'inactive' }
-    }
+      entry: "inline:toggle.active#entry[0]",
+      on: {
+        TOGGLE: 'inactive',
+        "Event 2": "New state 1"
+      }
+    },
+
+    "New state 1": {}
   }
 });
