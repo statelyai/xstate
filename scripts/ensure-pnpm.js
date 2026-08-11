3c0c4460f1883e119e4df9519c5b10b5de20c0f0
David Khourshid <davidkpiano@gmail.com>
2026-08-11 10:10:23 -0400

    Upgrade Changesets to v3

--- scripts/ensure-pnpm.js
+++ scripts/ensure-pnpm.js
@@ -1,3 +1,3 @@
-if (!/pnpm\/9/.test(process.env.npm_config_user_agent)) {
-  throw new Error('Please use `pnpm@^9` for installs.');
+if (!/pnpm\/10/.test(process.env.npm_config_user_agent)) {
+  throw new Error('Please use `pnpm@^10` for installs.');
 }
